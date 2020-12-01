de05be5836ce01f200b4b2c8229f74a9b3ce8ab2
davidsharp <hello@davidsharp.codes>
2020-12-01 09:28:13 +0000

    Completed 1-1

--- days/1.js
+++ days/1.js
@@ -1,4 +1,9 @@
-const part1 = () => {}
+const part1 = input => input.split('\n').map(Number).map(
+  (x,i,a)=>{
+    const y = a.filter(y=>x+y===2020)[0]
+    return y?x*y:null
+  }
+).filter(x=>x)[0]
 const part2 = null
 
 module.exports = {part1,part2}
