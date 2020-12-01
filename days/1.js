cb99bed757f834539d3dbdec5561ff53746d071a
davidsharp <hello@davidsharp.codes>
2020-12-01 09:37:20 +0000

    Completed 1-2 🎄

--- days/1.js
+++ days/1.js
@@ -4,6 +4,25 @@ const part1 = input => input.split('\n').map(Number).map(
     return y?x*y:null
   }
 ).filter(x=>x)[0]
-const part2 = null
+const part2 = input => {
+  const list = input.split('\n').map(Number)
+  let answer = 0
+  for(let i=0;i<list.length;i++){
+    for(let j=0;j<list.length;j++){
+      for(let k=0;k<list.length;k++){
+        if(
+          i!=j && i!=k && j!=k &&
+          list[i]+list[j]+list[k]===2020
+        ){
+          answer=list[i]*list[j]*list[k]
+          break;
+        }
+      }
+      if(answer)break;
+    }
+    if(answer)break;
+  }
+  return answer
+}
 
 module.exports = {part1,part2}
