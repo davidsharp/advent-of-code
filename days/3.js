506deeefa1bb49fc807fde84696e15423fdc3674
davidsharp <hello@davidsharp.codes>
2020-12-03 09:51:57 +0000

    Slimmed down 3-2

--- days/3.js
+++ days/3.js
@@ -13,20 +13,21 @@ const part1 = input => { //r3-d1
 const part2 = input => {
   const forest = parse(input)
   const routes = [
-    {inc: 1, treesHit: 0, x: 0, y:0}, //Right 1, down 1.
-    {inc: 3, treesHit: 0, x: 0, y:0}, //Right 3, down 1.
-    {inc: 5, treesHit: 0, x: 0, y:0}, //Right 5, down 1.
-    {inc: 7, treesHit: 0, x: 0, y:0}, //Right 7, down 1.
-    {inc: 1, treesHit: 0, x: 0, y:0, steep:true}, //Right 1, down 2.
+    {inc: 1}, //Right 1, down 1.
+    {inc: 3}, //Right 3, down 1.
+    {inc: 5}, //Right 5, down 1.
+    {inc: 7}, //Right 7, down 1.
+    {inc: 1, steep:true}, //Right 1, down 2.
   ]
   const width = forest[0].length
   const routeResults = routes.map(route=>{
-    let {x,y,treesHit,inc} = route
+    let x = 0
+    let y = 0
+    let treesHit = 0
     while(y<forest.length){
       if(forest[y][x%width]=='#')treesHit++
-      x+=inc
-      y++
-      if(route.steep)y++
+      x+=route.inc
+      y+=route.steep?2:1
     }
     return treesHit
   })
