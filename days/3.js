5e5fa9c1de9944886f20a51aa42e9f3a40f9f057
davidsharp <hello@davidsharp.codes>
2020-12-03 09:48:36 +0000

    Completed 3-2 🎄

--- days/3.js
+++ days/3.js
@@ -10,6 +10,27 @@ const part1 = input => { //r3-d1
   })
   return treesHit
 }
-const part2 = input => {}
+const part2 = input => {
+  const forest = parse(input)
+  const routes = [
+    {inc: 1, treesHit: 0, x: 0, y:0}, //Right 1, down 1.
+    {inc: 3, treesHit: 0, x: 0, y:0}, //Right 3, down 1.
+    {inc: 5, treesHit: 0, x: 0, y:0}, //Right 5, down 1.
+    {inc: 7, treesHit: 0, x: 0, y:0}, //Right 7, down 1.
+    {inc: 1, treesHit: 0, x: 0, y:0, steep:true}, //Right 1, down 2.
+  ]
+  const width = forest[0].length
+  const routeResults = routes.map(route=>{
+    let {x,y,treesHit,inc} = route
+    while(y<forest.length){
+      if(forest[y][x%width]=='#')treesHit++
+      x+=inc
+      y++
+      if(route.steep)y++
+    }
+    return treesHit
+  })
+  return routeResults.reduce((a,r)=>a?a*r:r,0)
+}
 
 module.exports = {part1,part2}
