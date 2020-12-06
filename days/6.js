ac6cc4f02b08559bd091ff2375dee12711b36e32
davidsharp <hello@davidsharp.codes>
2020-12-06 11:54:46 +0000

    Completed 6-2 🎄

--- days/6.js
+++ days/6.js
@@ -6,6 +6,22 @@ const part1 = input => {
     total + group.join('').split('').reduce((gTotal,q)=>gTotal.add(q),new Set()).size
   ),0)
 }
-const part2 = input => {}
+const part2 = input => {
+  const groups = parse(input)
+  return groups.map(group=>{
+    if(group.length==1) return group[0].length
+    const allYes = new Set()
+    const leader = group[0].split('').sort()
+    leader.forEach(
+      q => {
+        for(let i=1;i<group.length;i++){
+          if(group[i].indexOf(q)==-1)return;
+        }
+        allYes.add(q)
+      }
+    )
+    return allYes.size
+  }).reduce((a,b)=>a+b,0)
+}
 
 module.exports = {part1, part2}
