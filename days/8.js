b2cfdc74cfa160f2d17c5bcb6291927223ad5c2d
davidsharp <hello@davidsharp.codes>
2020-12-08 09:32:28 +0000

    Completed 8-2 🎄

--- days/8.js
+++ days/8.js
@@ -20,7 +20,39 @@ const part1 = input => {
   }
   return global
 }
-const part2 = input => {}
+const part2 = input => {
+  const instructions = parse(input)
+  let set = new Set()
+  const goal = instructions.length
+  set.add(goal)
+  return fromState({global:0,pointer:0,set,instructions,goal})[1]
+}
+const fromState = ({global,pointer,set:_set,instructions,goal,branched=false}) => {
+  const set = new Set(_set)
+  while(!set.has(pointer)){
+    set.add(pointer)
+    const {op,arg} = instructions[pointer]
+    if(op=='acc'){
+      global+=arg
+      pointer++;
+    }
+    if(op=='jmp'){
+      if(!branched){
+        const [success, _global] = fromState({global,pointer:pointer+1,set,instructions,goal,branched:true})
+        if(success){return [success,_global]}
+      }
+      pointer+=arg;
+    }
+    if(op=='nop'){
+      if(!branched){
+        const [success,_global] = fromState({global,pointer:pointer+arg,set,instructions,goal,branched:true})
+        if(success){return [success,_global]}
+      }
+      pointer++;
+    }
+  }
+  return [pointer==goal,global];
+}
 
 module.exports = { part1, part2 }
 
