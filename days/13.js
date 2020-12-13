9fa3f08b15f5ab26504fed5ff758351d20f70191
davidsharp <hello@davidsharp.codes>
2020-12-13 16:40:43 +0000

    Cleaned up 13-2

--- days/13.js
+++ days/13.js
@@ -24,16 +24,12 @@ const part1 = input => {
 
 const part2 = input => {
   const buses = parse2(input)
-  console.log('Buses :::',buses)
   let {start:timestamp} = buses.slice(1).reduce(({start,coprime,buses},bus)=>{
     let timestamp = start+coprime
     let found = false
     buses.push(bus)
-    console.log('---\nStarting from :::',start,'multiplyer :::',coprime)
     while(!found){
-      console.log('t :::', timestamp)
       found = buses.map(bus=>{
-        console.log('- bus :::',bus,'remainder :::',(timestamp+bus.offset+coprime)%bus.id)
         return (timestamp+bus.offset+coprime)%bus.id==0
       }).reduce((a,found)=>a&&found,true)
       timestamp+=coprime
@@ -50,42 +46,5 @@ const part2 = input => {
   })
   return timestamp
 }
-
-/*
-    ** Example: 7,13,x,x,59
-    **
-    ** Previous bus is 7, this bus is 13, with delay +1.
-    ** A time T is needed such that:
-    **      7x == T
-    **     13y == (T + 1)
-    **
-    ** Performing an iterative search for T on multiples of 7 and checking (T + 1)
-    ** eventually reveals that:
-    **   (7 * 11) == 77
-    **   (13 * 6) == 78
-    **
-    ** To find further times that match this condition, imagine some value W added to T.
-    **    7j == T + W
-    **   13k == (T + 1) + W
-    ** Substituting:
-    **    7j == 7x + W,  and j == x + (W / 7)
-    **   13k == 13y + W, and k == y + (W / 13)
-    ** For j and k to be integers, since x and y are integers, W must be a multiple of both 7 and 13.
-    ** Since all input numbers are conveniently prime, the smallest multiple of both 7 and 13 is (7 * 13).
-    ** Thus, W == (7 * 13) == 91.
-    **
-    **
-    ** Next, a time T is needed such that:
-    **      7x == T
-    **     13y == (T + 1)
-    **     59z == (T + 4)
-    **
-    ** Performing an iterative search from 77, adding multiples of 91, eventually reveals that:
-    **    (7 * 50) == 350
-    **   (13 * 27) == 351
-    **    (59 * 6) == 354
-    **
-    ** As above, the next T that matches this condition would be 350 + (7 * 13 * 59) == 350 + (5369) == 5719.
-    */
 
 module.exports = {part1,part2}
