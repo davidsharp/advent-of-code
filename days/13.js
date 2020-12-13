3394234f8be735d90ce2ee2917440a2d3c2553f9
davidsharp <hello@davidsharp.codes>
2020-12-13 13:51:17 +0000

    Inefficient 13-2 solution (works with examples)

--- days/13.js
+++ days/13.js
@@ -1,10 +1,18 @@
 const parse = input => {
   const [timestamp,buses] = input.split('\n')
-  return {timestamp,buses:buses.split(',').filter(b=>b!='x')}
+  return {timestamp,buses:(buses||'').split(',').filter(b=>b!='x')}
+}
+const parse2 = input => {
+  const [timestamp,buses] = input.split('\n')
+  return (buses||timestamp).split(',').map((bus,i)=>{
+    if(bus=='x')return null
+    return {id:Number(bus),offset:i}
+  }).filter(bus=>bus)
 }
 
 const part1 = input => {
   const {timestamp,buses} = parse(input)
+  if(buses.length==1) return 'no timestamp';
   const tTable = buses.map(bus=>{
     let circuits=0
     while((bus*circuits)<timestamp){circuits++}
@@ -14,4 +22,21 @@ const part1 = input => {
   return (earliestBus.earliest-timestamp) * earliestBus.id
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const buses = parse2(input)
+  let timestamp = buses[0].id
+  let result = null
+  //console.log(buses)
+  while(result==null){
+    const found = buses.map(bus=>{
+      //if(timestamp == 1068781) console.log(bus, timestamp+bus.offset, (timestamp+bus.offset)%bus.id)
+      return (timestamp+bus.offset)%bus.id==0
+    }).reduce((a,found)=>a&&found,true)
+    //if(timestamp == 1068781)console.log(found)
+    if(found)result=timestamp
+    else timestamp+=buses[0].id
+  }
+  return timestamp
+}
+
+module.exports = {part1,part2}
