407ed7445509a5525ce847fe199f47395d71974e
davidsharp <hello@davidsharp.codes>
2020-12-14 19:06:01 +0000

    Completed 14-2 🎄

--- days/14.js
+++ days/14.js
@@ -10,6 +10,18 @@ const parse = input => {
   })
 }
 
+const parse2 = input => {
+  let instructions = input.split('\n')
+  return instructions.map(i=>{
+    const addr = i.match(/mem\[([0-9]*)\]/)
+    if(addr){
+      const value = parseInt(i.split(' = ')[1])
+      return {addr:parseInt(addr[1]),value}
+    }
+    else return {mask: i.split(' = ')[1]}
+  })
+}
+
 const parseMask = mask => [...mask].reverse().map((digit,i)=>{
     if(digit=='X')return [i,'X']
     return [i,parseInt(digit)]
@@ -44,6 +56,31 @@ const part1 = input => {
   return mem.reduce((a,b)=>a+b,0)
 }
 
-const part2 = input => {}
+const part2 = input => {
+  const instructions = parse2(input)
+  let currMask = null
+  const mem = new Map()
+  instructions.forEach(inst=>{
+    if(inst.mask) currMask = inst.mask
+    else {
+      let addr = inst.addr.toString(2).padStart(36,0).split('');
+      // replace 1s and Xs
+      addr = addr.map((bit,i)=>{
+        if(currMask[i]=='1') return 1;
+        else if(currMask[i]=='X') return 'X';
+        else return bit;
+      }).join('')
+      const floaters = addr.split('X')
+      let addresses = floaters.reduce((acc,floater)=>{
+        if(acc.length==0) return [floater]
+        const ones = [...acc].map(x=>x+'0'+floater)
+        const zeroes = [...acc].map(x=>x+'1'+floater)
+        return [...ones,...zeroes]
+      },[])
+      addresses.forEach(addr=>mem.set(parseInt(addr,2),inst.value))
+    }
+  })
+  return [...mem.values()].reduce((a,b=0)=>a+b,0)
+}
 
 module.exports = {part1,part2}
