a5e91c7c106bd5532b22444a20961209a7b0fc0b
davidsharp <hello@davidsharp.codes>
2020-12-17 10:43:34 +0000

    Completed 17-2 🎄

--- 2020/days/17.js
+++ 2020/days/17.js
@@ -1,9 +1,3 @@
-/*const parse = input => {
-  const rows = input.split('\n').map(row=>row.split('').map(char=>char=='#'))
-  //state[z][y][x]
-  return [rows]
-}*/
-
 const parse = input => {
   const set = new Set()
   input.split('\n').forEach((row,y)=>{
@@ -31,13 +25,13 @@ const part1 = input => {
       for(let y=bounds[1][0];y<=bounds[1][1];y++){
         for(let z=bounds[2][0];z<=bounds[2][1];z++){
           let count = 0;
-          let neighbours = [];
+          //let neighbours = [];
             for(let _x=-1;_x<=1;_x++){
               for(let _y=-1;_y<=1;_y++){
                 for(let _z=-1;_z<=1;_z++){
                   if(!(_x==0&&_y==0&&_z==0)&&prevState.has(`${x+_x},${y+_y},${z+_z}`)){
                     count++
-                    neighbours.push([x+_x,y+_y,z+_z])
+                    //neighbours.push([x+_x,y+_y,z+_z])
                   }
                 }
               }
@@ -65,7 +59,67 @@ const part1 = input => {
   return state.size
 }
 
-module.exports = {part1}
+const parse2 = input => {
+  const set = new Set()
+  input.split('\n').forEach((row,y)=>{
+    row.split('').forEach((col,x)=>{if(col=='#')set.add(`${x},${y},0,0`)})
+  })
+  return {state:set,bounds:[[0,input.split('\n')[0].length],[0,input.split('\n').length],[0,0],[0,0]]}
+}
+
+const part2 = input => {
+  const {state,bounds} = parse2(input)
+  let turns = 0
+  while(turns<6){
+    const prevState = new Set(state)
+    // re-define cube boundaries
+    prevState.forEach(val=>{
+      const [x,y,z,w] = val.split(',').map(Number)
+      if(x-1<bounds[0][0])bounds[0][0]=x-1;
+      if(x+1>bounds[0][1])bounds[0][1]=x+1;
+      if(y-1<bounds[1][0])bounds[1][0]=y-1;
+      if(y+1>bounds[1][1])bounds[1][1]=y+1;
+      if(z-1<bounds[2][0])bounds[2][0]=z-1;
+      if(z+1>bounds[2][1])bounds[2][1]=z+1;
+      if(w-1<bounds[3][0])bounds[3][0]=w-1;
+      if(w+1>bounds[3][1])bounds[3][1]=w+1;
+    })
+    for(let x=bounds[0][0];x<=bounds[0][1];x++){
+      for(let y=bounds[1][0];y<=bounds[1][1];y++){
+        for(let z=bounds[2][0];z<=bounds[2][1];z++){
+          for(let w=bounds[3][0];w<=bounds[3][1];w++){
+            let count = 0;
+              for(let _x=-1;_x<=1;_x++){
+                for(let _y=-1;_y<=1;_y++){
+                  for(let _z=-1;_z<=1;_z++){
+                    for(let _w=-1;_w<=1;_w++){
+                      if(!(_x==0&&_y==0&&_z==0&&_w==0)&&prevState.has(`${x+_x},${y+_y},${z+_z},${w+_w}`)){
+                        count++
+                      }
+                    }
+                  }
+                }
+              }
+            if(prevState.has(`${x},${y},${z},${w}`)){
+              if(!(count==2||count==3)){
+                state.delete(`${x},${y},${z},${w}`)
+              }
+            }
+            else {
+              if(count==3){
+                state.add(`${x},${y},${z},${w}`)
+              }
+            }
+          }
+        }
+      }
+    }
+    turns++
+  }
+  return state.size
+}
+
+module.exports = {part1,part2}
 
 /*
 If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube becomes inactive.
