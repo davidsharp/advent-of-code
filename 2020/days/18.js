103d58048900f18fd0ae1269c881432e22df2787
davidsharp <hello@davidsharp.codes>
2020-12-18 11:28:03 +0000

    Solved 18-2, but the wrong way round

--- 2020/days/18.js
+++ 2020/days/18.js
@@ -33,7 +33,6 @@ const solveProblem = tokens => {
       total=total==null?token:op(total,token)
     }
   })
-  //console.log(tokens.join(''),'=',total)
   return total
 }
 const plus = (a,b) => (
@@ -45,6 +44,47 @@ const mult = (a,b) => (
   a*b
 )
 
-const log = (depth,...rest) => console.log(''.padStart('-',depth),...rest)
+const part2 = input => {
+  const problems = parse(input)
+  return problems.map(solveProblem2).reduce((a,b)=>a+b,0)
+}
 
-module.exports = {part1}
+const solveProblem2 = tokens => {
+  let parsedTokens = [];
+  let ignoreDepth = 0;
+  let terminated = null;
+  // parsing tokens
+  tokens.forEach((token,i)=>{
+    if(terminated)return;
+    if(token=='('){
+      if(ignoreDepth==0){
+        const innerResult = solveProblem2(tokens.slice(i+1))
+        parsedTokens.push(innerResult)
+      }
+      ignoreDepth++
+    }else if(token==')'){
+      ignoreDepth--
+      if(ignoreDepth<0)terminated=true
+    }else if(ignoreDepth!=0)return;
+    else parsedTokens.push(token)
+  })
+
+  console.log(parsedTokens)
+
+  //loop and multiply
+  while(parsedTokens.findIndex(x=>x=='*')>-1){
+    const found = parsedTokens.findIndex(x=>x=='*')
+    const multiplied = parsedTokens[found-1]*parsedTokens[found+1]
+    parsedTokens=[
+      ...parsedTokens.slice(0,found-1),
+      multiplied,
+      ...parsedTokens.slice(found+2)
+    ]
+  }
+  //loop and add
+  const solved = parsedTokens.join('').split('+').reduce((a,b)=>a+parseInt(b),0)
+
+  return solved
+}
+
+module.exports = {part1,part2}
