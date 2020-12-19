01e06f64e16aef3d4de383e0b94a7881181de19d
davidsharp <hello@davidsharp.codes>
2020-12-19 14:37:56 +0000

    Fleshing out a regexp builder for 19-1

--- 2020/days/19.js
+++ 2020/days/19.js
@@ -2,9 +2,9 @@ const parse = input => {
   let [rules,messages] = input.split('\n\n').map(section=>section.split('\n'))
   rules = rules.reduce((map,rule)=>{
     const [key, ...rules] = rule.split(/: | \| /)
-    map.set(key,rules.map(r=>{
+    map.set(Number(key),rules.map(r=>{
       if(/"[a-z]"/i.test(r)) return {char: r.match(/"([a-z])"/i)[1]}
-      else return {pointers: r.split(' ')}
+      else return {pointers: r.split(' ').map(Number)}
     }))
     return map
   },new Map())
@@ -15,7 +15,15 @@ const part1 = input => {
   const {rules,messages} = parse(input)
   console.log(rules)
   console.log(messages)
+  const bReg = buildRegex(rules)
+  console.log(new RegExp(bReg(0)).test(messages[0]))
   // contruct a regex from the rules?
+}
+
+const buildRegex = rules => key => {
+  const rule = rules.get(key)
+  const subrules = rule.map(subrule=>subrule.char||subrule.pointers.map(p=>buildRegex(rules)(p)))
+  return `(${subrules.flat().join('|')})`
 }
 
 module.exports = {part1}
