29b5cafa3961bfe0f78af70699c63580e22d9653
davidsharp <hello@davidsharp.codes>
2021-12-01 08:37:10 +0000

    Tidy up Day 1 (Just return the answer)

--- 2021/days/1.js
+++ 2021/days/1.js
@@ -2,7 +2,7 @@ const part1 = input => input.split('\n').map(Number).reduce(({last,inc},curr,i)=
   if(i==0) return {last:curr,inc:0}
   if(curr>last) return {last:curr, inc:inc+1}
   else return {last:curr, inc}
-},{})
+},{})?.inc
 
 const part2 = input => input.split('\n').map(Number).reduce(({last,inc},curr,i,a)=>{
   if(i>=a.length-2)return {last,inc}
@@ -10,6 +10,6 @@ const part2 = input => input.split('\n').map(Number).reduce(({last,inc},curr,i,a
   if(i==0) return {last:win,inc:0}
   if(win>last) return {last:win, inc:inc+1}
   else return {last:win, inc}
-},{})
+},{})?.inc
 
 module.exports = {part1, part2}
