b6750f6373e528b9b13d83ba18442a8658efbbd6
davidsharp <hello@davidsharp.codes>
2021-12-01 08:30:26 +0000

    ['21] Completed 1-1

--- 2021/days/1.js
+++ 2021/days/1.js
@@ -1,3 +1,7 @@
-const part1 = () => {}
+const part1 = input => input.split('\n').map(Number).reduce(({last,inc},curr,i)=>{
+  if(i==0) return {last:curr,inc:0}
+  if(curr>last) return {last:curr, inc:inc+1}
+  else return {last:curr, inc}
+},{})
 
 module.exports = {part1}
