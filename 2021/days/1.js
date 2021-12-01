78f7a9b3774905ae763404e824aa1c3e80d92315
davidsharp <hello@davidsharp.codes>
2021-12-01 08:36:07 +0000

    Completed 1-2! ☃️

--- 2021/days/1.js
+++ 2021/days/1.js
@@ -4,4 +4,12 @@ const part1 = input => input.split('\n').map(Number).reduce(({last,inc},curr,i)=
   else return {last:curr, inc}
 },{})
 
-module.exports = {part1}
+const part2 = input => input.split('\n').map(Number).reduce(({last,inc},curr,i,a)=>{
+  if(i>=a.length-2)return {last,inc}
+  const win = curr + a[i+1] + a[i+2]
+  if(i==0) return {last:win,inc:0}
+  if(win>last) return {last:win, inc:inc+1}
+  else return {last:win, inc}
+},{})
+
+module.exports = {part1, part2}
