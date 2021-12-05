5965c51e684d5bade0342952ec88fc875d5c4785
davidsharp <hello@davidsharp.codes>
2021-12-05 10:10:09 +0000

    Completed 5-2! ☃️

--- 2021/days/5.js
+++ 2021/days/5.js
@@ -40,4 +40,37 @@ const part1 = input => {
   return Object.values(ventPositions).filter(x=>x>1).length
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const lines = input.split('\n').map(line=>{
+   const [start, end] = line.split(' -> ').map(c=>{
+     const [x,y] = c.split(',').map(Number)
+     return {x,y}
+   })
+   return {start, end}
+ })
+
+ const ventPositions = {}
+ lines.forEach(line=>{
+   let {x,y} = line.start
+   const {end} = line
+   while(x!=end.x || y!=end.y){
+     //console.log(`${line.start.x},${line.start.y}->${end.x},${end.y}`,x,y)
+     ventPositions[`${x},${y}`] = (
+       ventPositions[`${x},${y}`]?
+         ventPositions[`${x},${y}`] + 1 : 1
+     )
+     if(x!=end.x)x<end.x?x++:x--
+     if(y!=end.y)y<end.y?y++:y--
+   }
+   // once more for the final position
+   ventPositions[`${x},${y}`] = (
+     ventPositions[`${x},${y}`]?
+       ventPositions[`${x},${y}`] + 1 : 1
+   )
+ })
+
+ // count dangerous vents
+ return Object.values(ventPositions).filter(x=>x>1).length
+}
+
+module.exports = {part1, part2}
