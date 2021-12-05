89bde7ac2e35a32d15a3d0937274e8499299021c
davidsharp <hello@davidsharp.codes>
2021-12-05 10:05:17 +0000

    Completed 5-1

---
+++ 2021/days/5.js
@@ -0,0 +1,43 @@
+const part1 = input => {
+   const lines = input.split('\n').map(line=>{
+    const [start, end] = line.split(' -> ').map(c=>{
+      const [x,y] = c.split(',').map(Number)
+      return {x,y}
+    })
+    return {start, end}
+  }).filter(line=>line.start.x==line.end.x||line.start.y==line.end.y)
+
+  const ventPositions = {}
+  lines.forEach(line=>{
+    let {x,y} = line.start
+    const {end} = line
+    // just being lazy and doing both
+    // since one'll basically no-op
+    while(x!=end.x){
+      //console.log(`${line.start.x},${line.start.y}->${end.x},${end.y}`,x,y)
+      ventPositions[`${x},${y}`] = (
+        ventPositions[`${x},${y}`]?
+          ventPositions[`${x},${y}`] + 1 : 1
+      )
+      x<end.x?x++:x--
+    }
+    while(y!=end.y){
+      //console.log(`${line.start.x},${line.start.y}->${end.x},${end.y}`,x,y)
+      ventPositions[`${x},${y}`] = (
+        ventPositions[`${x},${y}`]?
+          ventPositions[`${x},${y}`] + 1 : 1
+      )
+      y<end.y?y++:y--
+    }
+    // once more for the final position
+    ventPositions[`${x},${y}`] = (
+      ventPositions[`${x},${y}`]?
+        ventPositions[`${x},${y}`] + 1 : 1
+    )
+  })
+
+  // count dangerous vents
+  return Object.values(ventPositions).filter(x=>x>1).length
+}
+
+module.exports = {part1}
