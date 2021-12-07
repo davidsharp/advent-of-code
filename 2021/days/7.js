81f1792882d744fce49646e5f2ca5ae77f3d36cb
davidsharp <hello@davidsharp.codes>
2021-12-07 11:04:13 +0000

    Completed 7-2 ☃️

--- 2021/days/7.js
+++ 2021/days/7.js
@@ -16,4 +16,36 @@ const part1 = input => {
   return lowestFuel
 }
 
-module.exports = {part1}
+function* fuelCalc(){
+  let cost = 1
+  let step = 1
+  while(true){
+    yield cost
+    step++
+    cost+=step
+  }
+}
+
+const part2 = input => {
+  const crabs = input.split(',').map(Number).sort((a,b)=>a-b)
+  const min = crabs[0]
+  const max = crabs[crabs.length-1]
+
+  let lowestFuel = null
+  for(let i = min;i<=max;i++){
+    const consumes = crabs.reduce((fuel,crab)=>{
+      const calc = fuelCalc()
+      let distance = Math.abs(crab-i)
+      let cost = 0
+      while(distance-->0)cost=calc.next().value
+      return fuel+cost
+    },0)
+    //console.log(`Position ${i} uses ${consumes}`)
+    if(!lowestFuel || consumes<lowestFuel)
+      lowestFuel = consumes
+  }
+
+  return lowestFuel
+}
+
+module.exports = {part1, part2}
