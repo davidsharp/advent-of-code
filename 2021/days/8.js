5fdc35df53f16bf43d978bad9039ab4e4342a69d
davidsharp <hello@davidsharp.codes>
2021-12-08 10:41:24 +0000

    8-2 is getting there, I think

--- 2021/days/8.js
+++ 2021/days/8.js
@@ -8,4 +8,97 @@ const part1 = input => {
   )).length
 }
 
-module.exports = {part1}
+const part2 = input => {
+  // replaceAll just for example input
+  const panels = input.replaceAll('|\n','| ').split('\n')
+    .map(x=>x.split(' | ')
+      .map(x=>x.split(' ')
+        .map(y=>y.split('').sort().join('')
+    )))
+  //console.log(panels)
+  decode(panels[0])
+}
+
+/*
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+
+ lengths:
+  0 -> 6
+  1 -> 2
+  2 -> 5
+  3 -> 5
+  4 -> 4
+  5 -> 5
+  6 -> 6
+  7 -> 3
+  8 -> 7
+  9 -> 6
+*/
+
+const decode = panel => {
+  const [digits, display] = panel
+  const characters = {
+    1:digits.find(d=>d.length == 2),
+    4:digits.find(d=>d.length == 4),
+    7:digits.find(d=>d.length == 3),
+    8:digits.find(d=>d.length == 7),
+  }
+  const decodings = {}
+
+  // get the top bar
+  decodings['a'] = characters[7].split('').find(x=>characters[1].indexOf(x)==-1)
+
+  // get 0, 6 and 9
+  const _069 = digits.filter(d=>d.length == 6)
+  // 6 will be missing half of 1
+  characters[6] = _069.find(d=>{
+    const one = characters[1].split('')
+    return d.indexOf(one[0])==-1 || d.indexOf(one[1])==-1
+  })
+  // get 2, 3 and 5
+  const _235 = digits.filter(d=>d.length == 5)
+  // 5 will almost match 6
+  characters[5] = _235.find(d=>{
+    return characters[6].split('').filter(x=>d.indexOf(x)==-1).length==1
+  })
+
+  // 9 is like 5 + 1
+  characters[9] = _069.find(d=>{
+    return d == [...new Set([
+      ...characters[5].split(''),
+      ...characters[1].split('')
+    ])].sort().join('')
+  })
+
+  // 0 is not 6 or 9
+  characters[0] = _069.find(d=>d!=characters[6]&&d!=characters[9])
+
+  // 2 will also be missing half of 1
+  characters[2] = _235.find(d=>{
+    const one = characters[1].split('')
+    return d.indexOf(one[0])==-1 || d.indexOf(one[1])==-1
+  })
+
+  // 3 is not 2 or 5
+  characters[0] = _235.find(d=>d!=characters[2]&&d!=characters[5])
+
+  console.log(digits,display,characters,decodings)
+}
+
+module.exports = {part1,part2}
