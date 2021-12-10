62aaf3068d8cdafc487d1330393b7bff56c6dc7d
davidsharp <hello@davidsharp.codes>
2021-12-10 10:41:04 +0000

    Completed 10-2! ☃️

--- 2021/days/10.js
+++ 2021/days/10.js
@@ -18,6 +18,34 @@ const part1 = input => {
   },0)
 }
 
+const part2 = input => {
+  const chunks = input.split('\n').map(x=>x.split(''))
+  const scores = chunks.reduce((acc,chunk)=>{
+    let stack = []
+    let points = 0
+    let corrupted = false
+    let index = 0
+    while(index<chunk.length && !corrupted){
+      if(/\(|\[|{|</.test(chunk[index]))
+        stack.push(chunk[index])
+      else {
+        const open = stack.pop()
+        const matching = matchBrackets(open,chunk[index])
+        if(!matching) corrupted = true
+      }
+      index++
+    }
+    if(corrupted)return acc
+    while(stack.length) {
+      points*=5
+      points+=scores2[stack.pop()]
+    }
+    acc.push(points)
+    return acc
+  },[]).sort((a,b)=>a-b)
+  return scores[Math.floor(scores.length/2)]
+}
+
 const matchBrackets = (open,close) => (
     (open == '(' && close == ')') ||
     (open == '[' && close == ']') ||
@@ -32,4 +60,12 @@ const scores = {
   '>': 25137,
 }
 
-module.exports = {part1}
+// opening brackets for ease
+const scores2 = {
+  '(': 1,
+  '[': 2,
+  '{': 3,
+  '<': 4,
+}
+
+module.exports = {part1,part2}
