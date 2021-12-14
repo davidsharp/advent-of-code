743947eb2e38cfa75f3fd0b1bce761354364ae47
davidsharp <hello@davidsharp.codes>
2021-12-14 11:38:51 +0000

    Seemingly working 14-2, very slow

--- 2021/days/14.js
+++ 2021/days/14.js
@@ -14,7 +14,7 @@ const run = (input,turns) => {
   let [elems, insertionMap] = parse(input)
 
   while(turns-->0){
-    console.log(`-- turn ${40-turns} | length = ${elems.length} --`)
+    //console.log(`-- turn ${40-turns} | length = ${elems.length} --`)
     let newStr = ''
     for(let i = 0;i<elems.length;i++){
       const elem = elems[i]
@@ -32,7 +32,34 @@ const run = (input,turns) => {
   return elemCount.pop() - elemCount.shift()
 }
 
-const part1 = input => run(input,10)
-const part2 = input => run(input,40)
+const run2 = (input,turns) => {
+  let [elems, insertionMap] = parse(input)
+  let elemCount = [...(new Set(input.replace(/\n| |-|\>/g,'').split('')))]
+    .reduce((acc,c)=>(acc[c]=0,acc),{})
+  elems.split('').forEach((elem,i)=>{
+    console.log('forEach elem '+i)
+    elemCount[elem]+=1
+    const next = elems[i+1]
+    if(next) insert([elem,next],turns,insertionMap,elemCount)
+  })
+
+  elemCount = Object.values(elemCount).sort((a,b)=>a-b)
+
+  return elemCount.pop() - elemCount.shift()
+}
+
+const insert = (pair,turns,rules,count) => {
+  const [elem,next] = pair
+  const toInsert = rules?.[elem]?.[next]
+  if(toInsert && turns){
+    count[toInsert]+=1
+    insert([elem,toInsert],turns-1,rules,count)
+    insert([toInsert,next],turns-1,rules,count)
+  }
+}
+
+// running both to compare output
+const part1 = input => [run(input,10),run2(input,10)]
+const part2 = input => run2(input,40)
 
 module.exports = {part1,part2}
