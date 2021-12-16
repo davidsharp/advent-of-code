e85adf994515ce1f06c94a9aa3fe6a2394e493ad
davidsharp <hello@davidsharp.codes>
2021-12-16 11:01:58 +0000

    Completed 16-2! ☃️

--- 2021/days/16.js
+++ 2021/days/16.js
@@ -4,13 +4,15 @@ const decodeHex = hex => hex.split('').map(
 
 const part1 = input => {
   const packet = /[2-9A-Z]/.test(input)?decodeHex(input):input
-  console.log(packet)
-  return parsePacket(packet,0).version
+  return sumPacketVersion(packet,0).version
+}
+const part2 = input => {
+  const packet = /[2-9A-Z]/.test(input)?decodeHex(input):input
+  return parsePacket(packet,0).value
 }
 
 // for part1, returns version sum, and length
-// part2? parsePacket returns packet contents, and length
-const parsePacket = (packet,index) => {
+const sumPacketVersion = (packet,index) => {
   const version = parseInt(packet.substr(index,3),2)
   const typeID = parseInt(packet.substr(index+3,3),2)
   let readIndex = index+6
@@ -32,7 +34,7 @@ const parsePacket = (packet,index) => {
       readIndex+=16
       const endOfSub = readIndex+bitLength
       while(readIndex<endOfSub){
-        const {version,length} = parsePacket(packet,readIndex)
+        const {version,length} = sumPacketVersion(packet,readIndex)
         versionSum+=version
         readIndex+=length
       }
@@ -41,25 +43,64 @@ const parsePacket = (packet,index) => {
       let subPacketCount = parseInt(packet.substr(readIndex+1,11),2)
       readIndex+=12
       while(subPacketCount-->0){
-        const {version,length} = parsePacket(packet,readIndex)
+        const {version,length} = sumPacketVersion(packet,readIndex)
         versionSum+=version
         readIndex+=length
       }
     }
     return {version:version+versionSum,length:readIndex-index}
   }
-
-  /*
-  // outputs literal value (type 4)
-  let output = []
-  let packetEnded = false
-  while(!packetEnded){
-    if(packet[readIndex]=='0')packetEnded=true
-    output.push(packet.substr(readIndex+1,4))
-    readIndex+=5
-  }
-  return parseInt(output.join(''),2)
-  */
 }
 
-module.exports = {part1}
+// part2 - parsePacket returns packet contents, and length
+const parsePacket = (packet,index) => {
+  const version = parseInt(packet.substr(index,3),2)
+  const typeID = parseInt(packet.substr(index+3,3),2)
+  let readIndex = index+6
+
+  if(typeID == 4){
+    let packetEnded = false
+    let output = []
+    while(!packetEnded){
+      if(packet[readIndex]=='0')packetEnded=true
+      output.push(packet.substr(readIndex+1,4))
+      readIndex+=5
+    }
+    return {value:parseInt(output.join(''),2),length:readIndex-index}
+  }
+  else{
+    const I = packet[readIndex]
+    let subPackets = []
+    if(I=='0'){
+      const bitLength = parseInt(packet.substr(readIndex+1,15),2)
+      readIndex+=16
+      const endOfSub = readIndex+bitLength
+      while(readIndex<endOfSub){
+        const {value,length} = parsePacket(packet,readIndex)
+        subPackets.push(value)
+        readIndex+=length
+      }
+    }
+    if(I=='1'){
+      let subPacketCount = parseInt(packet.substr(readIndex+1,11),2)
+      readIndex+=12
+      while(subPacketCount-->0){
+        const {value,length} = parsePacket(packet,readIndex)
+        subPackets.push(value)
+        readIndex+=length
+      }
+    }
+    const length = readIndex - index
+    let value;
+    if(typeID == 0)value = subPackets.reduce((a,b)=>a+b)
+    if(typeID == 1)value = subPackets.reduce((a,b)=>a*b)
+    if(typeID == 2)value = Math.min(...subPackets)
+    if(typeID == 3)value = Math.max(...subPackets)
+    if(typeID == 5)value = subPackets[0]>subPackets[1]?1:0
+    if(typeID == 6)value = subPackets[0]<subPackets[1]?1:0
+    if(typeID == 7)value = subPackets[0]==subPackets[1]?1:0
+    return {value,length}
+  }
+}
+
+module.exports = {part1,part2}
