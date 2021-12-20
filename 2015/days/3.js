6ba5419c0cb61b1e26238be9f19f23feb801c1f8
davidsharp <hello@davidsharp.codes>
2021-12-20 19:17:57 +0000

    Completed 2015's 3-2 ☃️

--- 2015/days/3.js
+++ 2015/days/3.js
@@ -14,5 +14,24 @@ const part1 = input => {
   }
   return houses.size
 }
+const part2 = input => {
+  let i = 0
+  let x = 0
+  let y = 0
+  let roboX = 0
+  let roboY = 0
+  let houses = new Set(['0,0'])
+  while(i<input.length){
+    const robo = !(i%2)
+    const inst = input[i]
+    if(inst == '^') robo?roboY--:y--
+    if(inst == '>') robo?roboX++:x++
+    if(inst == 'v') robo?roboY++:y++
+    if(inst == '<') robo?roboX--:x--
+    houses.add(`${(robo?roboX:x)},${(robo?roboY:y)}`)
+    i++
+  }
+  return houses.size
+}
 
-module.exports = {part1}
+module.exports = {part1,part2}
