e9ddd08bf1057f34bbc77f933dff389a943df080
davidsharp <hello@davidsharp.codes>
2021-12-20 18:44:04 +0000

    Completed 2018's 1-2 ☃️🤷‍♂️

--- 2018/days/1.js
+++ 2018/days/1.js
@@ -1,3 +1,19 @@
 module.exports = {
-  part1: data=>data.split('\n').reduce((a,b)=>a+((b[0]=='+'?1:-1)*parseInt(b.slice(1))),0)
+  part1: data=>data.split('\n').reduce((a,b)=>a+((b[0]=='+'?1:-1)*parseInt(b.slice(1))),0),
+  part2: data=>{
+    const changes = data.split('\n')
+    let value = 0
+    let seen = new Set([0])
+    let seenTwice = null
+    let index = 0
+    while(!seenTwice){
+      const change = changes[index]
+      value+=((change[0]=='+'?1:-1)*parseInt(change.slice(1)))
+      index=(index+1)%changes.length
+      if(seen.has(value))seenTwice = value
+      seen.add(value)
+    }
+
+    return seenTwice
+  }
 }
