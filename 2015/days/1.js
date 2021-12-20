fa5a9b18dc47e82a54d4bfa00b74c3631a9b6522
davidsharp <hello@davidsharp.codes>
2021-12-20 18:54:39 +0000

    Completed 2015's 1-2 (in a dumb way) ☃️

--- 2015/days/1.js
+++ 2015/days/1.js
@@ -1,5 +1,13 @@
 const part1 = input => input.split('').reduce(
   (floor,inst) => floor+=(inst=='('?1:-1)
 ,0)
+const part2 = input => input.split('').reduce(
+  ({floor,minusOnePos},inst,i) => {
+    if(minusOnePos)return{minusOnePos}
+    floor+=(inst=='('?1:-1)
+    if(floor==-1)return{minusOnePos:i+1}
+    return {floor}
+  }
+,{floor:0}).minusOnePos
 
-module.exports = {part1}
+module.exports = {part1,part2}
