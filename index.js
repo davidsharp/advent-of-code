b0dfa7c03f089cd78d5aa36b850bdfe04de6e504
davidsharp <hello@davidsharp.codes>
2021-12-20 13:36:33 +0000

    Update run script to allow for saved input (as well as default piped input)

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
+const {readFile} = require('fs/promises')
+
+const thisYear = 2021
+
 // run
-const runDay = (data,day,year=2021) => {
+const run = async () => {
+  const [day, year] = [process.argv[2], process.argv[3]||thisYear]
+  let input;
+  try{ input = await readFile(`./${year}/input/${day}.txt`,'utf8') }
+  catch(e){/*fail silently*/}
+
+  // by default, read from stdin
+  const chunk = await readStdIn()
+  if (chunk !== null) {
+    if(chunk == '' || chunk == '\n') console.log('note: your pasted(?) input is empty')
+    runDay(chunk, day, year)
+  }
+  // else use input if there is one
+  else if(input) {
+    runDay(input, day, year)
+  }
+  else {
+    console.log(`No input data, either pipe input or create './${year}/input/${day}.txt'`)
+  }
+
+  // and exit once we're done
+  process.exit(1)
+}
+
+const readStdIn = () => (new Promise(resolve=>{
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', () => resolve(process.stdin.read()))
+  // resolve if there no immediate input
+  // 5ms should be more than enough for the event
+  setTimeout(()=>resolve(null),5)
+}))
+
+const runDay = (data,day,year=thisYear) => {
   if(parseInt(day)){
     const dayO = require(`./${year}/days/${day}`)
     if(dayO){
@@ -16,13 +52,6 @@ const runDay = (data,day,year=2021) => {
     else console.log('no solution for day '+day)
   } else console.log('no day specified')
 }
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    runDay(chunk, process.argv[2])
-  }
-});
 
 const { performance } = require('perf_hooks');
 
@@ -36,3 +65,5 @@ const runPart = (fn, data) => {
   console.log(`  ( took ${ms}.${fraction}ms )`)
   return output
 }
+
+run()
