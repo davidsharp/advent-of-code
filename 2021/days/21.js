b6a6f3cb721e03e7b1cb0d24465fd45a220c2f57
davidsharp <hello@davidsharp.codes>
2021-12-21 11:25:40 +0000

    Possibly completed 21-2? (sloooow)

--- 2021/days/21.js
+++ 2021/days/21.js
@@ -34,7 +34,46 @@ const part1 = input => {
   }
 
   return Math.min(players[0].score,players[1].score)*rolls
-
 }
 
-module.exports = {part1}
+const part2 = input => {
+  // player positions
+  let players = input.split('\n')
+    .map(line=>({
+      score:0,
+      position:Number(line.split(': ')[1])-1
+    }))
+
+  return playGame(clone(players))
+}
+const playGame = (players,playerTurn=0,roll=0,rollValue=0) => {
+  const boardLength = 10
+  const goal = 21
+
+  if(roll==2){
+    players[playerTurn].position += rollValue
+    players[playerTurn].position %= boardLength
+    // board is 1-indexed, so plus 1
+    players[playerTurn].score += (players[playerTurn].position+1)
+
+    // check for winner
+    if(players[playerTurn].score>goal)return playerTurn==0?[1,0]:[0,1]
+
+    // next turn
+    roll = 0
+    rollValue = 0
+    playerTurn = (playerTurn+1)%2
+  } else roll++
+
+  const wins = [
+    playGame(clone(players),playerTurn,roll,rollValue+1),
+    playGame(clone(players),playerTurn,roll,rollValue+2),
+    playGame(clone(players),playerTurn,roll,rollValue+3)
+  ].reduce(([aa,ab],[ba,bb])=>[aa+ba,ab+bb],[0,0])
+
+  return wins
+}
+
+const clone = obj => JSON.parse(JSON.stringify(obj))
+
+module.exports = {part1,part2}
