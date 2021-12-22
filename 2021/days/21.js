3524c5ae8787aa024099ebf56063261a7c525408
davidsharp <hello@davidsharp.codes>
2021-12-22 10:01:41 +0000

    I suspect this 21-2 might run faster?

--- 2021/days/21.js
+++ 2021/days/21.js
@@ -46,34 +46,52 @@ const part2 = input => {
 
   return playGame(clone(players))
 }
-const playGame = (players,playerTurn=0,roll=0,rollValue=0) => {
+const playGame = (players,playerTurn=0,rollValue=0,multiplier) => {
   const boardLength = 10
   const goal = 21
 
-  if(roll==2){
+  // if rollValue == 0, initing, so jump ahead
+  if(rollValue>0){
     players[playerTurn].position += rollValue
     players[playerTurn].position %= boardLength
     // board is 1-indexed, so plus 1
     players[playerTurn].score += (players[playerTurn].position+1)
 
     // check for winner
-    if(players[playerTurn].score>goal)return playerTurn==0?[1,0]:[0,1]
+    if(players[playerTurn].score>=goal)return playerTurn==0?[1,0]:[0,1]
 
     // next turn
-    roll = 0
     rollValue = 0
     playerTurn = (playerTurn+1)%2
-  } else roll++
+  }
 
   const wins = [
-    playGame(clone(players),playerTurn,roll,rollValue+1),
-    playGame(clone(players),playerTurn,roll,rollValue+2),
-    playGame(clone(players),playerTurn,roll,rollValue+3)
+    // rather than making branches for each roll
+    // pre-calculate possible paths
+    //  and time by how often the turn result shows up
+    playGame(clone(players),playerTurn,3,1),
+    playGame(clone(players),playerTurn,4,3),
+    playGame(clone(players),playerTurn,5,6),
+    playGame(clone(players),playerTurn,6,7),
+    playGame(clone(players),playerTurn,7,6),
+    playGame(clone(players),playerTurn,8,3),
+    playGame(clone(players),playerTurn,9,1),
   ].reduce(([aa,ab],[ba,bb])=>[aa+ba,ab+bb],[0,0])
 
-  return wins
+  return wins.map(x=>x*multiplier)
 }
 
 const clone = obj => JSON.parse(JSON.stringify(obj))
 
 module.exports = {part1,part2}
+
+/*
+  pre-calculated all the possible results of the rolls per turn:
+  3s	1
+  4s	3
+  5s	6
+  6s	7
+  7s	6
+  8s	3
+  9s	1
+*/
