4dd6e56d867f0416fec9ab018e5c825fc4ac5924
David Sharp <hello@davidsharp.codes>
2022-12-03 12:18:13 +0000

    Completed 3-2! ☃️

--- 2022/days/3.js
+++ 2022/days/3.js
@@ -13,4 +13,25 @@ const part1 = input => input.split('\n').map(
   }
 ).reduce((a,c)=>a+c)
 
-module.exports = {part1}
+const part2 = input => {
+  const rucksacks = input.split('\n')
+  const groups = []
+  let currentGroup = []
+  rucksacks.forEach(r=>{
+    currentGroup.push(r.split(''))
+    if(currentGroup.length==3){
+      groups.push(currentGroup)
+      currentGroup = []
+    }
+  })
+  return groups.map(
+    g => {
+      const item = g[0].find(x=>g[1].find(y=>x==y)&&g[2].find(y=>x==y))
+      const match = item.charCodeAt()
+      let value = match - lower <= 0 ? match - upper + 27 : match - lower
+      return value
+    }
+  ).reduce((a,c)=>a+c)
+}
+
+module.exports = {part1, part2}
