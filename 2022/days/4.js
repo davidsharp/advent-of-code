cfbd5724e8c391c6dd553ca42e86d6b45d6c3f55
David Sharp <hello@davidsharp.codes>
2022-12-04 11:17:06 +0000

    Completed 4-1

---
+++ 2022/days/4.js
@@ -0,0 +1,12 @@
+const part1 = input => input.split('\n').map(
+  pair => {
+    // pair of elves like: [[start,end],[start,end]]
+    pair = pair.split(',').map(e => e.split('-').map(Number)).sort(
+      // sorting so longest is first
+      (a,b) => a[1]-a[0]>b[1]-b[0]?-1:1
+    )
+    return pair[0][0]<=pair[1][0] && pair[0][1]>=pair[1][1]
+  }
+).reduce((a,c)=>c?a+1:a,0)
+
+module.exports = {part1}
