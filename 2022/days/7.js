47dcfd085ee292e5fb579de407228b67f48e805c
davidsharp <hello@davidsharp.codes>
2022-12-07 11:07:15 +0000

    Tidied up 7-2 🎁

--- 2022/days/7.js
+++ 2022/days/7.js
@@ -1,4 +1,4 @@
-const part1 = input => {
+const getDirectorySizes = input => {
   const fs = {}
   const loc = []
   input.split('\n').map(x=>x.split(' '))
@@ -22,36 +22,7 @@ const part1 = input => {
   const sizes = {}
   read(fs,'/',sizes)
 
-  return Object.entries(sizes).filter(x=>x[1]<=100_000).reduce((a,c)=>a+c[1],0)
-}
-
-const part2 = input => {
-  const fs = {}
-  const loc = []
-  input.split('\n').map(x=>x.split(' '))
-  .forEach(
-    c => {
-      if(c[0]=='$'){
-        if(c[1]=='cd'){
-          if(c[2]=='..')loc.pop()
-          else loc.push(c[2])
-        } // else ls
-      }
-      // else ls'ing
-      else {
-        if(!fs[loc.join('/')])fs[loc.join('/')]={}
-        if(c[0]!='dir') fs[loc.join('/')][c[1]] = Number(c[0])
-        else fs[loc.join('/')][c[1]] = 'dir'
-      }
-    }
-  )
-
-  const sizes = {}
-  read(fs,'/',sizes)
-
-  const toFree = 30000000 - (70000000 - sizes['/'])
-
-  return Object.entries(sizes).filter(x=>x[1]>=toFree).sort((a,b)=>a[1]>b[1]?1:-1)[0][1]
+  return sizes
 }
 
 const read = (fs,dir,obj) => {
@@ -61,6 +32,18 @@ const read = (fs,dir,obj) => {
 
   obj[dir] = size
   return size
+}
+
+const part1 = input => (
+  Object.entries(getDirectorySizes(input)).filter(x=>x[1]<=100_000).reduce((a,c)=>a+c[1],0)
+)
+
+
+const part2 = input => {
+  const sizes = getDirectorySizes(input)
+  const toFree = 30000000 - (70000000 - sizes['/'])
+
+  return Object.entries(sizes).filter(x=>x[1]>=toFree).sort((a,b)=>a[1]>b[1]?1:-1)[0][1]
 }
 
 module.exports = {part1,part2}
