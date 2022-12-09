c27355fe90be98f498a4a22280e4379f67e3b11c
David Sharp <hello@davidsharp.codes>
2022-12-09 10:50:59 +0000

    Completed 9-2! ☃️

--- 2022/days/9.js
+++ 2022/days/9.js
@@ -54,4 +54,62 @@ const part1 = input => {
   return visited.size
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const h = [0,0]
+  const knots = Array(9).fill(0).map(_=>[0,0])
+
+  const visited = new Set(['0,0'])
+
+  const move = dir => {
+    // 0 = x, 1 = y
+    const axis = /R|L/.test(dir)?0:1
+    const mag = /U|L/.test(dir)?-1:1
+    h[axis]+=mag
+    console.log('h: ',h)
+  }
+
+  const follow = (h,t) => {
+    const xDiff = Math.abs(h[0]-t[0])
+    const yDiff = Math.abs(h[1]-t[1])
+
+    // close enough, don't move
+    if(xDiff + yDiff <= 1 || (xDiff == 1 && yDiff == 1)) return;
+    // horizontal / vertical
+    if(xDiff == 0 || yDiff == 0){
+      if(h[0]>t[0])t[0]++
+      else if(h[1]>t[1])t[1]++
+      else if(h[0]<t[0])t[0]--
+      else t[1]--
+    }
+    // else diagonal
+    else {
+      const _t = [...t]
+      if(h[0]>t[0])_t[0]++
+      if(h[1]>t[1])_t[1]++
+      if(h[0]<t[0])_t[0]--
+      if(h[1]<t[1])_t[1]--
+      t[0] = _t[0]
+      t[1] = _t[1]
+    }
+
+    console.log('t: ',t)
+  }
+
+  input.split('\n').forEach(
+    inst => {
+      let [dir,by] = inst.split(' ')
+      while(by-->0){
+        console.log(dir,by+1)
+        move(dir)
+        knots.forEach(
+          (x,i)=>follow(knots[i-1]||h,x)
+        )
+        visited.add(knots[8].join(','))
+      }
+    }
+  )
+
+  return visited.size
+}
+
+module.exports = {part1,part2}
