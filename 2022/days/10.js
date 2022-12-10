4c5aad929040369e3e32e89d857d500231e020ec
David Sharp <hello@davidsharp.codes>
2022-12-10 19:33:28 +0000

    Completed 10-2! ☃️

--- 2022/days/10.js
+++ 2022/days/10.js
@@ -5,7 +5,6 @@ const part1 = input => {
   let watchedCycles = []
   const instructions = input.split('\n')
   while(instructions.length>0 || buffer){
-    console.log(cycle,x)
     if((cycle%40)==20) watchedCycles.push([cycle,x])
     if(!buffer){
       const inst = instructions.shift()
@@ -21,9 +20,40 @@ const part1 = input => {
     }
     cycle++
   }
-  console.log(cycle,x)
 
   return watchedCycles.slice(0,6).reduce((a,[c,x])=>a+(c*x),0)
 }
 
-module.exports = {part1}
+const part2 = input => {
+  let cycle = 1
+  let x = 1
+  let buffer = 0
+  let line = '#'
+  let lines = []
+  const instructions = input.split('\n')
+  while(instructions.length>0 || buffer){
+    if(!buffer){
+      const inst = instructions.shift()
+      if(inst=='noop'){
+        // no-op
+      }
+      else {
+        buffer = Number(inst.split(' ')[1])
+      }
+    } else {
+      x += buffer
+      buffer = 0
+    }
+    const pos = cycle%40
+    line = line + (pos>=x-1&&pos<=x+1?'#':'.')
+    cycle++
+    if(cycle%40==0){
+      lines.push(line)
+      line = ''
+    }
+  }
+
+  return ['',...lines].join('\n')
+}
+
+module.exports = {part1,part2}
