e287e5df3a3463303f66caae6841dd8266f4e29d
David Sharp <hello@davidsharp.codes>
2022-12-11 12:48:46 +0000

    11-2 attempt, very slow

--- 2022/days/11.js
+++ 2022/days/11.js
@@ -12,10 +12,9 @@ const part1 = input => {
       }
     }
   )
-console.log(monkeys)
+
   let round = 1
   while(round<=20){
-    console.log(round)
     monkeys.forEach(
       m => {
         let items = [...m.items]
@@ -30,7 +29,51 @@ console.log(monkeys)
         )
       }
     )
-    console.log('after',round,':',monkeys)
+    round++
+  }
+
+  return monkeys.map(m=>m.inspections)
+    .sort((a,b)=>a>b?-1:1).slice(0,2).reduce((a,c)=>a*c)
+}
+
+const part2 = input => {
+  const monkeys = input.split('\n\n').map(
+    m => {
+      let [,items,op,test,t,f] = m.split('\n')
+      items = items.split(': ')[1].split(', ').map(Number)
+      op = op.split('= old ')[1].split(' ')
+      op[1] = op[1]=='old'?'old':Number(op[1])
+      return {
+        items,op,
+        test:[test,t,f].map(x=>Number(x.split(' ').pop())),
+        inspections:0
+      }
+    }
+  )
+
+  let round = 1
+  while(round<=10_000){
+    if(round%10==0)console.log('round: ',round)
+    monkeys.forEach(
+      m => {
+        let items = [...m.items]
+        m.items = []
+        m.inspections += items.length
+        const n = i => (m.op[1]=='old'?i:BIify(i,m.op[1]))
+        items = items.map(i=>{
+          // they don't get smaller, but just in case?
+          if(isBI(i)&&i<Number.MAX_SAFE_INTEGER)i=Number(i)
+          let x = m.op[0] == '+' ? i + n(i) : i * n(i)
+          if(x==Infinity)
+            x = m.op[0] == '+' ? BigInt(i) + BigInt(n(i))
+              : BigInt(i) * BigInt(n(i))
+          return x
+        })
+        items.forEach(
+          i => monkeys[i%(BIify(i,m.test[0]))==0?m.test[1]:m.test[2]].items.push(i)
+        )
+      }
+    )
     round++
   }
 
@@ -40,4 +83,7 @@ console.log(monkeys)
     .sort((a,b)=>a>b?-1:1).slice(0,2).reduce((a,c)=>a*c)
 }
 
-module.exports = {part1}
+const isBI = i => typeof i == 'bigint'
+const BIify = (i,n) => isBI(i)?BigInt(n):n
+
+module.exports = {part1,part2}
