d7ed8968fd0b568bdcace3251b24a88754ded13c
David Sharp <hello@davidsharp.codes>
2022-12-11 13:36:24 +0000

    Start a new 11-2 solution

--- 2022/days/11.js
+++ 2022/days/11.js
@@ -40,9 +40,10 @@ const part2 = input => {
   const monkeys = input.split('\n\n').map(
     m => {
       let [,items,op,test,t,f] = m.split('\n')
-      items = items.split(': ')[1].split(', ').map(Number)
+      // storing ints as arrays: 42 -> [4,2]
+      items = items.split(': ')[1].split(', ').map(x=>x.split('').map(Number))
       op = op.split('= old ')[1].split(' ')
-      op[1] = op[1]=='old'?'old':Number(op[1])
+      op[1] = Number(op[1])||'old'
       return {
         items,op,
         test:[test,t,f].map(x=>Number(x.split(' ').pop())),
@@ -53,37 +54,48 @@ const part2 = input => {
 
   let round = 1
   while(round<=10_000){
-    if(round%10==0)console.log('round: ',round)
     monkeys.forEach(
       m => {
         let items = [...m.items]
         m.items = []
         m.inspections += items.length
-        const n = i => (m.op[1]=='old'?i:BIify(i,m.op[1]))
-        items = items.map(i=>{
-          // they don't get smaller, but just in case?
-          if(isBI(i)&&i<Number.MAX_SAFE_INTEGER)i=Number(i)
-          let x = m.op[0] == '+' ? i + n(i) : i * n(i)
-          if(x==Infinity)
-            x = m.op[0] == '+' ? BigInt(i) + BigInt(n(i))
-              : BigInt(i) * BigInt(n(i))
-          return x
-        })
+        const n = i => (m.op[1]=='old'?i:m.op[1])
+        // TODO - if + old, double, if * odl, square
+        items = items.map(i=>((
+          m.op[0] == '+'? add(i,n(i)) : mult(i,n(i))
+        )))
         items.forEach(
-          i => monkeys[i%(BIify(i,m.test[0]))==0?m.test[1]:m.test[2]].items.push(i)
+          i => monkeys[mod(i,m.test[0])==0?m.test[1]:m.test[2]].items.push(i)
         )
       }
     )
     round++
   }
 
-  console.log(monkeys)
-
   return monkeys.map(m=>m.inspections)
     .sort((a,b)=>a>b?-1:1).slice(0,2).reduce((a,c)=>a*c)
 }
 
-const isBI = i => typeof i == 'bigint'
-const BIify = (i,n) => isBI(i)?BigInt(n):n
+const add = (num,i) => {
+  num[num.length-1] += i
+  return massage(num)
+}
+const mult = (num,i) => {}
+const mod = (num,i) => {} // long division, grab remainder
+const double = (num) => {}
+const square = (num) => {}
+const massage = num => {
+  num.reverse()
+  let i = 0
+  while(i<num.length){
+    if(num[i]>10){
+      if(!num[i+1]) num.push(Math.floor(num/10))
+      else num[i+1] += Math.floor(num/10)
+      num[i]%=10
+    }
+  }
+  num.reverse()
+  return num
+}
 
 module.exports = {part1,part2}
