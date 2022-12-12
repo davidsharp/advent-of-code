97931e45978043df7669c1a67bc97493d1ed5d53
David Sharp <hello@davidsharp.codes>
2022-12-12 10:53:59 +0000

    closer?

--- 2022/days/12.js
+++ 2022/days/12.js
@@ -22,69 +22,53 @@ const crawl = (grid, pos, _set = []) => {
   const set = new Set([..._set])
   set.add(pos.join(','))
 
-  console.log(set.size)
+  console.log(set)
 
   const current = grid[y][x]=='S'?1:grid[y][x]
 
   let attempts = []
   //right
-  if (x + 1 < w && grid[y][x + 1]) {
+  if (x + 1 < w && grid[y][x + 1] && !set.has([x+1,y].join(','))) {
     if (grid[y][x + 1] == 'E') { return set }
     else if (grid[y][x + 1] != 'S' && (current + 1) >= grid[y][x + 1]) {
       let a = crawl(grid, [x + 1, y], set)
-      if (a) {
-        attempts.push(a.map(
-          s => (
-            s.size + set.size == new Set([...s, ...set]) ? new Set([...s, ...set]) : false
-          )
-        ))
+      if (a && a.size + set.size == new Set([...a, ...set])){
+          attempts.push(new Set([...a, ...set]))
       }
     }
   }
   //up
-  if (y - 1 >= 0 && grid[y - 1][x]) {
+  if (y - 1 >= 0 && grid[y - 1][x] && !set.has([x,y-1].join(','))) {
     if (current == 26 && grid[y - 1][x] == 'E') { return set }
     else if (grid[y - 1][x] != 'S' && (current + 1) >= grid[y - 1][x]) {
       let a = crawl(grid, [x, y - 1], set)
-      if (a) {
-        attempts.push(a.map(
-          s => (
-            s.size + set.size == new Set([...s, ...set]) ? new Set([...s, ...set]) : false
-          )
-        ))
+      if (a && a.size + set.size == new Set([...a, ...set])){
+          attempts.push(new Set([...a, ...set]))
       }
     }
   }
   //down
-  if (y + 1 < h && grid[y + 1][x]) {
+  if (y + 1 < h && grid[y + 1][x] && !set.has([x,y+1].join(','))) {
     if (current == 26 && grid[y + 1][x] == 'E') { return set }
     else if (grid[y + 1][x] != 'S' && (current + 1) >= grid[y + 1][x]) {
       let a = crawl(grid, [x, y + 1], set)
-      if (a) {
-        attempts.push(a.map(
-          s => (
-            s.size + set.size == new Set([...s, ...set]) ? new Set([...s, ...set]) : false
-          )
-        ))
+      if (a && a.size + set.size == new Set([...a, ...set])){
+          attempts.push(new Set([...a, ...set]))
       }
     }
   }
   //left
-  if (x - 1 >= 0 && grid[y][x - 1]) {
+  if (x - 1 >= 0 && grid[y][x - 1] && !set.has([x-1,y].join(','))) {
     if (current == 26 && grid[y][x - 1] == 'E') { return set }
     else if (grid[y][x - 1] != 'S' && (current + 1) >= grid[y][x - 1]) {
       let a = crawl(grid, [x - 1, y], set)
-      if (a) {
-        attempts.push(a.map(
-          s => (
-            s.size + set.size == new Set([...s, ...set]) ? new Set([...s, ...set]) : false
-          )
-        ))
+      if (a && a.size + set.size == new Set([...a, ...set])){
+          attempts.push(new Set([...a, ...set]))
       }
     }
   }
 
-  attempts = attempts.flat().filter(x => x)
+  attempts = attempts.filter(x => x)
   if (attempts.length == 0) return false
   else return attempts.sort((a,b)=>a.size>b.size?1:-1)[0]
 }
