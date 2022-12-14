40ed29698868048a21e7dde270f5267b7853c241
davidsharp <hello@davidsharp.codes>
2022-12-14 10:43:34 +0000

    Completed 14-1

--- 2022/days/14.js
+++ 2022/days/14.js
@@ -1,6 +1,7 @@
 const part1 = input => {
   const lines = input.split('\n').map(l=>l.split(' -> ').map(p=>p.split(',').map(Number)))
   const points = new Set()
+  let lowest = 0
   lines.forEach(vertices => {
     let frm = vertices.pop()
     let to
@@ -9,15 +10,34 @@ const part1 = input => {
       let [x1,y1] = frm
       let [x2,y2] = to
       while(x1!=x2||y1!=y2){
+        if(y1>lowest)lowest=y1
         points.add([x1,y1].join(','))
         if(x1!=x2)x1+=(x1>x2?-1:1)
         if(y1!=y2)y1+=(y1>y2?-1:1)
       }
       points.add([x1,y1].join(','))
+      if(y1>lowest)lowest=y1
       frm = to
     }
   })
-  return points
+
+  let sandCount = 0
+  let sand = {x:500,y:0}
+  while(sand.y<lowest){
+    if(!points.has([sand.x,sand.y+1].join(',')))
+      sand.y++
+    else if(!points.has([sand.x-1,sand.y+1].join(',')))
+      {sand.y++;sand.x--}
+    else if(!points.has([sand.x+1,sand.y+1].join(',')))
+      {sand.y++;sand.x++}
+    else {
+      points.add([sand.x,sand.y,].join(','))
+      sandCount++
+      sand = {x:500,y:0}
+    }
+  }
+
+  return sandCount
 }
 
 module.exports = {part1}
