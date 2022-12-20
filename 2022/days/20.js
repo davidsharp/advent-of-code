8a8e7d7b452371aa369a4147ec1ef26980b897b2
David Sharp <hello@davidsharp.codes>
2022-12-20 12:18:43 +0000

    closer

--- 2022/days/20.js
+++ 2022/days/20.js
@@ -8,17 +8,22 @@ const part1 = input => {
 
   init.forEach(
     ({val,ind},i) => {
+      //console.log(i,val,init.sort((a,b)=>a.ind>b.ind?1:-1))
       const newInd = (ind+val+init.length)%init.length
       init[i].ind = newInd
+      console.log(val,': ',ind,'->',init[i].ind)
       init.forEach(({ind:_ind},ii)=>{
         if(i==ii)return;
         //out of range, uneffected
         if(_ind<newInd && _ind<ind);
         if(_ind>newInd && _ind>ind);
 
-        if(_ind<=newInd && _ind>ind)init[i].ind=(init[i].ind-1+init.length%init.length);
-        if(_ind>=newInd && _ind<ind)init[i].ind=(init[i].ind+1+init.length%init.length);
+        if(_ind<=newInd && _ind>ind)init[ii].ind=(init[ii].ind-1+init.length%init.length);
+        if(_ind>=newInd && _ind<ind)init[ii].ind=(init[ii].ind+1+init.length%init.length);
       })
+
+      console.log(i,val,init.sort((a,b)=>a.ind>b.ind?1:-1))
+      console.log('---')
     }
   )
 
