c4dde1f8fb6f6c68a0c006f2cd8e0e8e9e0a8d20
David Sharp <hello@davidsharp.codes>
2023-12-01 12:14:40 +0000

    Too low?

--- 2023/days/1.js
+++ 2023/days/1.js
@@ -6,14 +6,20 @@ const part1 = input => input.split('\n').map(
 
 const nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].map(x=>new RegExp(x, "g"))
 const replace = str => {
-  let new_str = str
-  nums.forEach((c,i)=>{new_str=new_str.replace(c,i+1)})
-  return new_str
+  const new_str = str
+  const temp = []
+  nums.forEach((re,i)=>{
+    const idx = new_str.search(re)
+    if(idx>=0) temp.push([idx,i+1])
+  })
+  const new_nums = new_str.split('')
+  temp.forEach(([idx,val])=> new_nums[idx] = String(val))
+  return new_nums.filter(Number)
 }
 const part2 = input => input.split('\n').map(
-  str => replace(str).split('').filter(Number) // no zeroes, so this works
-)/*.reduce(
+  str => replace(str) // no zeroes, so this works
+).reduce(
   (acc,c) => acc + Number(c[0]+c[c.length-1])
-,0)*/
+,0)
 
 module.exports = { part1, part2 }
