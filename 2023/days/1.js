fdf065b080f508967e314974711ce3413635cb40
David Sharp <hello@davidsharp.codes>
2023-12-01 12:28:22 +0000

    Re-shuffling 1-2 slightly

--- 2023/days/1.js
+++ 2023/days/1.js
@@ -5,19 +5,18 @@ const part1 = input => input.split('\n').map(
 ,0)
 
 const nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].map(x=>new RegExp(x, "g"))
-const replace = str => {
-  const new_str = str
-  const temp = []
-  nums.forEach((re,i)=>{
-    const idx = [...new_str.matchAll(re)].map(({index})=>index)
-    idx.forEach(_idx => temp.push([_idx,i+1]))
-  })
-  const new_nums = new_str.split('')
-  temp.forEach(([idx,val])=> new_nums[idx] = String(val))
-  return new_nums.filter(Number)
-}
 const part2 = input => input.split('\n').map(
-  str => replace(str) // no zeroes, so this works
+  str => {
+    const new_str = str
+    const temp = []
+    nums.forEach((re,i)=>{
+      const idx = [...new_str.matchAll(re)].map(({index})=>index)
+      idx.forEach(_idx => temp.push([_idx,i+1]))
+    })
+    const new_nums = new_str.split('')
+    temp.forEach(([idx,val])=> new_nums[idx] = String(val))
+    return new_nums.filter(Number)
+  }
 ).reduce(
   (acc,c) => acc + Number(c[0]+c[c.length-1])
 ,0)
