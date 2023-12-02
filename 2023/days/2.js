8e6b5ec58bb9d22d18803e7d354a5a31459113c4
David Sharp <hello@davidsharp.codes>
2023-12-02 20:18:48 +0000

    Moved shared code between parts into a helper function

--- 2023/days/2.js
+++ 2023/days/2.js
@@ -1,15 +1,15 @@
 const p1_limit = {red: 12, green: 13, blue: 14}
+const parseRounds = line => line.split(': ').pop().split('; ').map(
+  round => round.split(', ').reduce((obj,draw)=>{
+    const [count, colour] = draw.split(' ')
+    obj[colour] = Number(count)
+    return obj
+  },{red:0,green:0,blue:0})
+)
 const part1 = input => input.split('\n').map(
   (line,i) => {
     const id = i + 1
-    const rounds = line.split(': ').pop().split('; ').map(
-      round => round.split(', ').reduce((obj,draw)=>{
-        const [count, colour] = draw.split(' ')
-        obj[colour] = Number(count)
-        return obj
-      },{red:0,green:0,blue:0})
-    )
-    return rounds.reduce((possible,round)=>(
+    return parseRounds(line).reduce((possible,round)=>(
       possible &&
       round.red <= p1_limit.red &&
       round.green <= p1_limit.green &&
@@ -19,22 +19,13 @@ const part1 = input => input.split('\n').map(
 ).reduce((a,b)=>a+b)
 
 const part2 = input => input.split('\n').map(
-  line => {
-    const rounds = line.split(': ').pop().split('; ').map(
-      round => round.split(', ').reduce((obj,draw)=>{
-        const [count, colour] = draw.split(' ')
-        obj[colour] = Number(count)
-        return obj
-      },{red:0,green:0,blue:0})
-    )
-    return rounds.reduce((current,round)=>(
-      {
-        red: Math.max(current.red,round.red),
-        green: Math.max(current.green,round.green),
-        blue: Math.max(current.blue,round.blue),
-      }
-    ),{red:0,green:0,blue:0})
-  }
+  line => parseRounds(line).reduce((current,round)=>(
+    {
+      red: Math.max(current.red,round.red),
+      green: Math.max(current.green,round.green),
+      blue: Math.max(current.blue,round.blue),
+    }
+  ),{red:0,green:0,blue:0})
 ).reduce((acc,{red,green,blue})=>(acc+(red*green*blue)),0)
 
 module.exports = { part1, part2 }
