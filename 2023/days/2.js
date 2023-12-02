653df37cbce148c4e9c396712b6858efdf24ef26
David Sharp <hello@davidsharp.codes>
2023-12-02 20:08:45 +0000

    [2023] Completed 2-2! 🎁

--- 2023/days/2.js
+++ 2023/days/2.js
@@ -20,4 +20,24 @@ const part1 = input => input.split('\n').map(
   }
 ).reduce((a,b)=>a+b)
 
-module.exports = { part1 }
+const part2 = input => input.split('\n').map(
+  (line,i) => {
+    const id = i + 1
+    const rounds = line.split(': ').pop().split('; ').map(
+      round => round.split(', ').reduce((obj,draw)=>{
+        const [count, colour] = draw.split(' ')
+        obj[colour] = Number(count)
+        return obj
+      },{red:0,green:0,blue:0})
+    )
+    return rounds.reduce((current,round)=>{
+      return {
+        red: Math.max(current.red,round.red),
+        green: Math.max(current.green,round.green),
+        blue: Math.max(current.blue,round.blue),
+      }
+    },{red:0,green:0,blue:0})
+  }
+).reduce((acc,{red,green,blue})=>(acc+(red*green*blue)),0)
+
+module.exports = { part1, part2 }
