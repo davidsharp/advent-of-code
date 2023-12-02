0b0490476fee97f78fa2daa0193b6f8e639387ed
David Sharp <hello@davidsharp.codes>
2023-12-02 20:12:16 +0000

    Cleaning up day 2 a little

--- 2023/days/2.js
+++ 2023/days/2.js
@@ -9,20 +9,17 @@ const part1 = input => input.split('\n').map(
         return obj
       },{red:0,green:0,blue:0})
     )
-    return rounds.reduce((possible,round)=>{
-      return (
-        possible &&
-        round.red <= p1_limit.red &&
-        round.green <= p1_limit.green &&
-        round.blue <= p1_limit.blue
-      )
-    },true) ? id : 0
+    return rounds.reduce((possible,round)=>(
+      possible &&
+      round.red <= p1_limit.red &&
+      round.green <= p1_limit.green &&
+      round.blue <= p1_limit.blue
+    ),true) ? id : 0
   }
 ).reduce((a,b)=>a+b)
 
 const part2 = input => input.split('\n').map(
-  (line,i) => {
-    const id = i + 1
+  line => {
     const rounds = line.split(': ').pop().split('; ').map(
       round => round.split(', ').reduce((obj,draw)=>{
         const [count, colour] = draw.split(' ')
@@ -30,13 +27,13 @@ const part2 = input => input.split('\n').map(
         return obj
       },{red:0,green:0,blue:0})
     )
-    return rounds.reduce((current,round)=>{
-      return {
+    return rounds.reduce((current,round)=>(
+      {
         red: Math.max(current.red,round.red),
         green: Math.max(current.green,round.green),
         blue: Math.max(current.blue,round.blue),
       }
-    },{red:0,green:0,blue:0})
+    ),{red:0,green:0,blue:0})
   }
 ).reduce((acc,{red,green,blue})=>(acc+(red*green*blue)),0)
 
