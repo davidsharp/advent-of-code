ded2e335d51b8af111755f9a0c1340c4667e9e02
David Sharp <hello@davidsharp.codes>
2023-12-03 18:03:13 +0000

    3-2 also too low 🤔

--- 2023/days/3.js
+++ 2023/days/3.js
@@ -10,12 +10,6 @@ const part1 = input => {
       for(let j = 0;j < cols.length;j++){
         if(Number(cols[j]) > -1){ // test for NaN vs 0
           current_num = current_num + cols[j]
-          /*console.log(
-            cols[j],
-            (rows[i-1] && !re.test(rows[i-1].substring(j-1,j+2).padEnd(3,'.'))) ,
-            !re.test(rows[i].substring(j-1,j+2).padEnd(3,'.')) ,  // pad to handle row ends
-            (rows[i+1] && !re.test(rows[i+1].substring(j-1,j+2).padEnd(3,'.')))
-          )*/
           if(
             !is_adjacent && 
             (
@@ -28,7 +22,6 @@ const part1 = input => {
           }
         }
         else {
-          //if(nums.length<10 && current_num.length)console.log(current_num,is_adjacent)
           if(is_adjacent)
             nums.push(Number(current_num))
           current_num = ''
@@ -45,4 +38,48 @@ const part1 = input => {
   return nums.reduce((a,b)=>a+b)
 }
 
-module.exports = { part1 }
+const part2 = input => {
+  const rows = input.split('\n')
+  const gears = {} // {"[x,y]": <Set>}
+  rows.forEach(
+    (row,i) => {
+      const cols = [...row]
+      let current_num = ''
+      let seen_gears = new Set()
+      for(let j = 0;j < cols.length;j++){
+        if(Number(cols[j]) > -1){ // test for NaN vs 0
+          current_num = current_num + cols[j]
+          for(let r = i-1;r<(i+2);r++){
+            if(rows[r]){
+              for(let c = j-1;c<(j+2);c++){
+                if(rows[r][c] == '*'){
+                  seen_gears.add([c,r].join())
+                }
+              }
+            }
+          }
+        }
+        else {
+          if(current_num)seen_gears.forEach(
+            gear => {
+              if(!gears[gear]) gears[gear] = new Set()
+              gears[gear].add(current_num)
+            }
+          )
+          current_num = ''
+          seen_gears = new Set()
+        }
+      }
+      // row end
+      if(current_num)seen_gears.forEach(
+        gear => {
+          if(!gears[gear]) gears[gear] = new Set()
+          gears[gear].add(current_num)
+        }
+      )
+    }
+  )
+  return Object.values(gears).filter(gear=>gear.size==2).map(gear=>[...gear]).reduce((acc,[a,b])=>acc+(a*b),0)
+}
+
+module.exports = { part1, part2 }
