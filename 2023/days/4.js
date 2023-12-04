8a56fc9c312031147d3c3162c02ce36c3e608561
David Sharp <hello@davidsharp.codes>
2023-12-04 11:52:18 +0000

    Refactors 4-2 down with a helper function

--- 2023/days/4.js
+++ 2023/days/4.js
@@ -1,15 +1,4 @@
-const part1 = input => input.split('\n').map(x=>x.split(/: +/).pop()).map(
-  card => {
-    const [winning,numbers] = card.split(/ +\| +/).map(x=>x.split(/ +/).map(Number))
-    let matches = 0
-    for(const num of winning){
-      if(numbers.includes(num)) matches++
-    }
-    return Math.floor(2**(matches-1))
-  }
-).reduce((a,b)=>a+b)
-
-const part2 = input => input.split('\n').map(x=>x.split(/: +/).pop()).map(
+const parseCards = input => input.split('\n').map(x=>x.split(/: +/).pop()).map(
   card => {
     const [winning,numbers] = card.split(/ +\| +/).map(x=>x.split(/ +/).map(Number))
     let matches = 0
@@ -18,12 +7,16 @@ const part2 = input => input.split('\n').map(x=>x.split(/: +/).pop()).map(
     }
     return matches
   }
-).reduce((stock,matches,i,a)=>{
+)
+
+const part1 = input => parseCards(input).reduce((a,b)=>a+Math.floor(2**(b-1)))
+
+const part2 = input => parseCards(input).reduce(({stock,total},matches,i,a)=>{
   if(!stock) stock = a.map(_=>1)
   for(var x = 1;x<=matches;x++){
     stock[i+x] += stock[i]
   }
-  return stock
-},null).reduce((a,b)=>a+b)
+  return {stock,total:total+stock[i]}
+},{stock:null,total:0}).total
 
 module.exports = { part1, part2 }
