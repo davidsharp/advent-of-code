1963c43d9f511c207723abc2498c76520cb8c9c2
David Sharp <hello@davidsharp.codes>
2023-12-05 11:35:28 +0000

    [2023] Completed 5-1

--- 2023/days/5.js
+++ 2023/days/5.js
@@ -1,31 +1,35 @@
 const part1 = input => {
   let [seeds, ...maps] = input.split('\n\n')
   seeds = seeds.split(': ').pop().split(' ').map(Number)
-  maps = maps.map(m => m.split(': ').pop().split('\n').map(
+  maps = maps.map(m => m.split(': ').pop().split('\n').slice(1).map(
       line => {
         const [dest_start,source_start,length] = line.split(' ').map(Number)
         return {dest_start,source_start,length}
       }
     )
   )
-  //console.log({seeds,maps})
+  //console.log(seeds,maps)
   return seeds.map(seed => {
-    let found = false
-    let line = 0
-    let map = maps[0]
-    while(!found && line<map.length){
-      const {source_start,length} = map[line]
-      if(seed >= source_start && seed < (source_start+length))
-        found = true
-      else
-        line++
+    let val = seed
+    //console.log('seed: ',seed)
+    for(const map of maps){
+      let found = false
+      let line = 0
+      while(!found && line<map.length){
+        const {source_start,length} = map[line]
+        if(val >= source_start && val < (source_start+length))
+          found = true
+        else
+          line++
+      }
+      if(found) {
+        const {dest_start,source_start} = map[line]
+        val = (val - source_start) + dest_start
+      }
+      //console.log({found,line,map:map[line],val})
     }
-    if(!found) return seed
-    else {
-      const {dest_start,source_start} = map[line]
-      return (seed - source_start) + dest_start
-    }
-  })
+    return val
+  }).reduce((a,b)=>Math.min(a,b))
 }
 
 module.exports = { part1 }
