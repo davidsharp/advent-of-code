d06744e2f3f4e00ffece8529097a72cc4a438b97
David Sharp <hello@davidsharp.codes>
2023-12-06 10:21:25 +0000

    [2023] Completed 6-1

---
+++ 2023/days/6.js
@@ -0,0 +1,15 @@
+const part1 = input => {
+  const [times, distances] = input.split('\n').map(line => line.split(/: +/).pop().split(/ +/))
+  const races = times.map((time,i)=>({time,distance:distances[i]}))
+  return races.reduce((acc,{time,distance},i,a)=>{
+    let hold = time - 1 // boat doesn't move if held down the whole time
+    let wins = 0
+    while(hold>0){
+      if((time-hold)*hold>distance)wins++
+      hold--
+    }
+    return wins*acc
+  },1)
+}
+
+module.exports = { part1 }
