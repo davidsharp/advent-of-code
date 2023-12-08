e2dbfdf5323c8c89b89fdf0ced9f3a3d98835ac8
David Sharp <hello@davidsharp.codes>
2023-12-08 18:30:21 +0000

    Naive 8-2 "solution"

--- 2023/days/8.js
+++ 2023/days/8.js
@@ -18,4 +18,28 @@ const part1 = input => {
   return step
 }
 
-module.exports = { part1 }
+const part2 = input => {
+  let [instructions,nodes] = input.split('\n\n')
+  nodes = nodes.split('\n').reduce(
+    (obj,line) => {
+      const [key, left, right] = line.split(/\W+/)
+      obj[key] = {left,right}
+      return obj
+    }
+  ,{})
+  let locations = Object.keys(nodes).filter(x=>/A$/.test(x))
+  let step = 0
+  console.log(locations)
+  while(!locations.reduce((notZ,loc)=>notZ&&/Z$/.test(loc),true)){
+    locations = locations.map(
+      location => instructions[step%instructions.length]=='L'?
+        nodes[location].left:
+        nodes[location].right
+    )
+    console.log(step,locations)
+    step++
+  }
+  return step
+}
+
+module.exports = { /*part1,*/ part2 }
