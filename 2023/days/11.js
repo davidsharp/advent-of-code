657597bd3f888a637617338dba0e229cab100386
David Sharp <hello@davidsharp.codes>
2023-12-11 10:42:36 +0000

    get galaxies

--- 2023/days/11.js
+++ 2023/days/11.js
@@ -19,7 +19,20 @@ const part1 = input => {
   let empty_row = []
   grid.forEach((row,i)=>{if(!row.includes('#'))empty_row.push(i)})
 
-  return {empty_col,empty_row}
+  let galaxies = []
+  let row_offset = 0
+  for(let row = 0;row<grid.length;row++){
+    let col_offset = 0
+    if(empty_row.includes(row))row_offset++
+    for(let col = 0;col<grid[row].length;col++){
+      if(empty_col.includes(col))col_offset++
+      if(grid[row][col]=='#'){
+        galaxies.push([row+row_offset,col+col_offset])
+      }
+    }
+  }
+
+  return {galaxies,empty_col,empty_row}
 }
 
 module.exports ={ part1 }
