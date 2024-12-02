899a9231a94886e3171c1938e5a61797ddfcc3d7
David Sharp <hello@davidsharp.codes>
2024-12-02 13:14:54 +0000

    De-duplicate shared code

--- 2024/days/2.js
+++ 2024/days/2.js
@@ -2,40 +2,31 @@ const parse = input => {
   return input.split('\n').map(l=>l.split(' ').map(Number))
 }
 
+const checkReport = report => {
+  const {safe} = report.reduce(({safe, ascending}, level, i, levels) => {
+    if(!safe) return {safe, ascending}
+    if (i == 0) { ascending = levels[i+1] > level }
+    else {
+      const prev = levels[i-1]
+      if (ascending) { safe = level > prev && level < prev + 4 }
+      else { safe = level < prev && level > prev - 4 }
+    }
+    return {safe, ascending}
+  },{safe:true,ascending:true})
+  return safe
+}
+
 const part1 = input => {
   const reports = parse(input)
 
   return reports.reduce((safeReports, report) => {
-    const {safe} = report.reduce(({safe, ascending}, level, i, levels) => {
-      if(!safe) return {safe, ascending}
-      if (i == 0) { ascending = levels[i+1] > level }
-      else {
-        const prev = levels[i-1]
-        if (ascending) { safe = level > prev && level < prev + 4 }
-        else { safe = level < prev && level > prev - 4 }
-      }
-      return {safe, ascending}
-    },{safe:true,ascending:true})
+    const safe = checkReport(report)
     return safeReports += (safe?1:0)
   },0)
 }
 
 const part2 = input => {
   const reports = parse(input)
-
-  const checkReport = report => {
-    const {safe} = report.reduce(({safe, ascending}, level, i, levels) => {
-      if(!safe) return {safe, ascending}
-      if (i == 0) { ascending = levels[i+1] > level }
-        else {
-          const prev = levels[i-1]
-          if (ascending) { safe = level > prev && level < prev + 4 }
-            else { safe = level < prev && level > prev - 4 }
-        }
-      return {safe, ascending}
-    },{safe:true,ascending:true})
-    return safe
-  }
 
   return reports.reduce((safeReports, report) => {
     let safe = checkReport(report)
