94596a82b8e72bdd59e715282e44cb57c890add3
David Sharp <hello@davidsharp.codes>
2024-12-02 10:04:41 +0000

    WIP, 2-2

--- 2024/days/2.js
+++ 2024/days/2.js
@@ -1,5 +1,5 @@
 const parse = input => {
-  return input.split('\n').map(l=>l.split(' ').map(Number))
+  return input.split('\n').slice(0,5).map(l=>l.split(' ').map(Number))
 }
 
 const part1 = input => {
@@ -20,4 +20,28 @@ const part1 = input => {
   },0)
 }
 
-module.exports = { part1 }
+const part2 = input => {
+  const reports = parse(input)
+
+  return reports.reduce((safeReports, report) => {
+    const {safe} = report.reduce(({safe, ascending, skip}, level, i, levels) => {
+      if(!safe) return {safe, ascending}
+      if (i == 0) { ascending = levels[i+1] > level }
+      else {
+        const prev = levels[i-(skip===i-1?2:1)]
+        let tempSafe = true
+        if (ascending) { tempSafe = level > prev && level < prev + 4 }
+        else { tempSafe = level < prev && level > prev - 4 }
+        if (!tempSafe && !skip) {
+          safe = true
+          skip = i
+        }
+        else { safe = tempSafe }
+      }
+      return {safe, ascending, skip}
+    },{safe:true,ascending:true,skip:null})
+    return safeReports += (safe?1:0)
+  },0)
+}
+
+module.exports = { part1, part2 }
