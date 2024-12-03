fc7987c1b07a8f3fee33c030e50481158715b3ed
David Sharp <hello@davidsharp.codes>
2024-12-03 09:17:41 +0000

    [2024] Completed 3-2! 🎄

--- 2024/days/3.js
+++ 2024/days/3.js
@@ -4,4 +4,19 @@ const part1 = input => input.match(/mul\(\d+,\d+\)/g)
     return acc + (a*b)
   },0)
 
-module.exports = {part1}
+const part2 = input => {
+  const instructions = input.match(/do(n't)?\(\)|mul\(\d+,\d+\)/g)
+  let enabled = true
+  let sum = 0
+  instructions.forEach(instruction => {
+    if (instruction == 'do()') enabled = true
+    else if (instruction == 'don\'t()') enabled = false
+      else if (enabled) {
+      const [a,b] = instruction.match(/\d+,\d+/)[0].split(',').map(Number)
+      sum += (a*b)
+    }
+  })
+  return sum
+}
+
+module.exports = {part1, part2}
