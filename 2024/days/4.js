e284918940c9e8a43d5f91f84a4ab821e2f760f8
David Sharp <hello@davidsharp.codes>
2024-12-04 10:17:37 +0000

    [2024] Completed 4-2! 🎄

--- 2024/days/4.js
+++ 2024/days/4.js
@@ -8,6 +8,17 @@ const part1 = input => {
   })
   return sum
 }
+const part2 = input => {
+  const lines = input.split('\n').map(line=>line.split(''))
+  let sum = 0
+  lines.forEach((line,y)=>{
+    line.forEach((char,x)=>{
+      if(char=='A') sum += onAFound(lines,x,y)
+    })
+  })
+  return sum
+}
+
 const onXFound = (lines,x,y) => {
   let sum = 0
   for(let i=-1;i<=1;i++){
@@ -25,5 +36,22 @@ const onXFound = (lines,x,y) => {
   }
   return sum
 }
+const onAFound = (lines,x,y) => {
+  let xMas = false
+  if(
+    // check space exists
+    lines[y-1] && lines[y+1] && lines[y][x-1] && lines[y][x+1] &&
+    // do they differ?
+    lines[y-1][x-1]!==lines[y+1][x+1] &&
+    lines[y-1][x+1]!==lines[y+1][x-1] &&
+    // are they S and M?
+    [lines[y-1][x-1],lines[y+1][x+1],lines[y+1][x-1],lines[y-1][x+1]].reduce((SorM,l)=>(
+      SorM && (l == 'S' || l == 'M')
+    ),true)
+  ){
+    xMas = true
+  }
+  return xMas
+}
 
-module.exports = {part1}
+module.exports = {part1,part2}
