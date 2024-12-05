082d6bf787e88f0d5831760b3d34080a2ce4d25e
David Sharp <hello@davidsharp.codes>
2024-12-05 10:14:18 +0000

    [2024] Completed 5-2! 🎄

--- 2024/days/5.js
+++ 2024/days/5.js
@@ -25,4 +25,30 @@ const part1 = input => {
   },0)
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const { rules, updates } = parse(input)
+  return updates.reduce((acc, update) => {
+    let correct = true
+    for (let i = 0; i < update.length && correct; i++) {
+      if (rules[update[i]]) {
+        for (let y of rules[update[i]]) {
+          // -1 is still correct
+          let yIdx = update.indexOf(y)
+          if (yIdx < i && yIdx > -1) correct = false;
+        }
+      }
+    }
+    const reorderedUpdate = update.toSorted((a, b) => {
+      if (rules[a]) {
+        for (let y of rules[a]) {
+          if(b==y) return 1
+        }
+      }
+      // otherwise correct order
+      return -1
+    })
+    return acc + (correct?0:Number(reorderedUpdate[Math.floor(update.length/2)]))
+  },0)
+}
+
+module.exports = {part1, part2}
