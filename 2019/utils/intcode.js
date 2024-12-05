d72121f1de2209dcd2097a6af13dc38255cd3a0a
David Sharp <hello@davidsharp.codes>
2024-12-05 23:15:29 +0000

    Intcode interpreter now works with 5-1

--- 2019/utils/intcode.js
+++ 2019/utils/intcode.js
@@ -1,35 +1,73 @@
 // used in 2, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25
 
 class Interpreter {
+  static instructions = {
+    1: 'add',
+    2: 'mult',
+    3: 'in',
+    4: 'out',
+    99: 'end',
+  }
+  // parameter modes
+  static MODES = {
+    POSITION: 0,
+    IMMEDIATE: 1,
+  }
   halted = false
   pointer = 0 // instruction pointer
   cells = [] // {}?
-  instructions = {
-    1: 'add',
-    2: 'mult',
-    99: 'end',
+  input = null
+  output = console.log //default
+  constructor(input, output) {
+    this.cells = input.split(',').map(Number)
+    if(output) this.output = output
   }
-  constructor(input) { this.cells = input.split(',').map(Number) }
   run() { while (!this.halted) {
-    const instruction = this.instructions[this.cells[this.pointer]]
-    this[instruction]()
+    const [inst,modes] = this.parse(this.cells[this.pointer])
+    const instruction = Interpreter.instructions[inst]
+    this[instruction](modes)
   } }
+  pipe(value) { this.input = value }
   step(x=1) {
     this.pointer += x
     if (this.pointer > this.cells.length) {
       this.halted = true
     }
   }
+  parse(value) {
+    const instruction = value % 100
+    let temp = Math.floor(value/100)
+    const parameterModes = []
+    while (temp>0) {
+      parameterModes.push(temp%10)
+      temp = Math.floor(temp/10)
+    }
+    return [instruction,parameterModes]
+  }
   poke(idx,value) { this.cells[idx] = value }
-  add() {
+  get(value, mode) {
+    if(mode == Interpreter.MODES.IMMEDIATE)
+      return value
+    else // Interpreter.MODES.POSITION
+      return this.cells[value]
+  }
+  add(modes) {
     const [a,b,out] = this.cells.slice(this.pointer+1,this.pointer+4)
-    this.cells[out] = this.cells[a] + this.cells[b]
+    this.cells[out] = this.get(a,modes[0]) + this.get(b,modes[1])
     this.step(4)
   }
-  mult() {
+  mult(modes) {
     const [a,b,out] = this.cells.slice(this.pointer+1,this.pointer+4)
-    this.cells[out] = this.cells[a] * this.cells[b]
+    this.cells[out] = this.get(a,modes[0]) * this.get(b,modes[1])
     this.step(4)
+  }
+  in() {
+    this.cells[this.cells[this.pointer+1]] = this.input
+    this.step(2)
+  }
+  out(modes) {
+    this.output(this.get(this.cells[this.pointer+1],modes[0]))
+    this.step(2)
   }
   end() { this.halted = true }
   result() { return this.cells[0] }
