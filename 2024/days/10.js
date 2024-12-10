7dfa58789aad1934e7bf78ea393ad055cceaae8e
David Sharp <hello@davidsharp.codes>
2024-12-10 11:44:51 +0000

    [2024] Completed 10-2! 🎄

--- 2024/days/10.js
+++ 2024/days/10.js
@@ -1,4 +1,4 @@
-const part1 = input => {
+const parse = input => {
   const map = input.split('\n').map(x=>x.split('').map(Number))
   const trailheads = []
   for (let y = 0; y < map.length; y++) {
@@ -6,6 +6,11 @@ const part1 = input => {
       if (map[y][x] == 0) trailheads.push([x,y])
     }
   }
+  return {map, trailheads}
+}
+
+const part1 = input => {
+  const {map, trailheads} = parse(input)
   const crawl = (h,x,y) => {
     let peaks = new Set()
     const directions = [[0,-1],[0,1],[-1,0],[1,0]]
@@ -27,4 +32,25 @@ const part1 = input => {
   },0)
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const {map, trailheads} = parse(input)
+  let peaks = {}
+  const crawl = (h,x,y) => {
+    const directions = [[0,-1],[0,1],[-1,0],[1,0]]
+    for (let [dx, dy] of directions) {
+      const point = map[y+dy]?.[x+dx]
+      if(point && point == h+1){
+        if (h+1 == 9) {
+          const k = `${x+dx},${y+dy}`
+          if(!peaks[k]) peaks[k] = 0
+          peaks[k]++
+        }
+        else crawl(h+1,x+dx,y+dy)
+      }
+    }
+  }
+  trailheads.forEach(([x,y])=>crawl(0,x,y))
+  return Object.values(peaks).reduce((a,b)=>a+b)
+}
+
+module.exports = {part1,part2}
