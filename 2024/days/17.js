5ceebfcfc39f5a36c38754493312a7445d2b5a6e
David Sharp <hello@davidsharp.codes>
2024-12-17 10:40:21 +0000

    17-2 working with example

--- 2024/days/17.js
+++ 2024/days/17.js
@@ -68,35 +68,25 @@ class Computer {
   cdv() { this._dv('c') }
 }
 const parse = input => input.split('\n\n').map(x=>x.match(/\d+\b/g).map(Number))
-const part1Tests = () => {
-  let c = new Computer([0,0,9],[2,6])
-  c.run()
-  console.assert(c.registers.b == 1, 'test 1')
-  c = new Computer([10,0,0],[5,0,5,1,5,4])
-  c.run()
-  console.assert(c.output.join(',') == '0,1,2', 'test 2')
-  c = new Computer([2024,0,0],[0,1,5,4,3,0])
-  c.run()
-  console.assert(c.output.join(',') == '4,2,5,6,7,7,7,7,3,1,0', 'test 3a')
-  console.assert(c.registers.a == 0, 'test 3b')
-  c = new Computer([0,29,0],[1,7])
-  c.run()
-  console.assert(c.registers.b == 26, 'test 4')
-  c = new Computer([0,2024,43690],[4,0])
-  c.run()
-  console.assert(c.registers.b == 44354, 'test 5')
 
-  c = new Computer([729,0,0],[0,1,5,4,3,0])
-  c.debug = true
-  c.run()
-  console.assert(c.output.join(',') == '4,6,3,5,6,3,5,2,1,0', 'example')
-}
 const part1 = input => {
   const [registers, instructions] = parse(input)
-  //part1Tests()
-  //console.log({registers,instructions})
   const comp = new Computer(registers,instructions)
   return comp.run().join(',')
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const [registers, instructions] = parse(input)
+  const [,b,c] = registers
+  let a = 0
+  let p
+  while (p != instructions.join(',')) {
+    console.log(a)
+    const com = new Computer([a,b,c],instructions)
+    p = com.run().join(',')
+    a++
+  }
+  return a-1
+}
+
+module.exports = {part1,part2}
