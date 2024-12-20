a0f0d5ce5d88791ac122f1f256d380b5ba6d6628
David Sharp <hello@davidsharp.codes>
2024-12-20 10:28:19 +0000

    [2024] Completed 20-1

---
+++ 2024/days/20.js
@@ -0,0 +1,47 @@
+const parse = input => {
+  const map = input.split('\n').map(x=>x.split(''))
+  const s = map.reduce((coords,row,y) => {
+    const foundS = row.indexOf('S')
+    if (foundS >= 0) coords = [foundS,y]
+    return coords
+  },[0,0])
+  const path = [s]
+  let e = null
+  const directions = [[0,-1],[0,1],[-1,0],[1,0]]
+  while (!e) {
+    const [x,y] = path[path.length-1]
+    for (let [dx, dy] of directions) {
+      if (map[y + dy]?.[x + dx] == 'E') {
+        e = [x+dx,y+dy]
+        path.push(e)
+      }
+      else if (map[y + dy]?.[x + dx] == '.') {
+        if (
+          !path.find(([_x,_y])=>((_x==x+dx)&&(_y==y+dy)))
+        ) {
+          path.push([x+dx,y+dy])
+        }
+      }
+    }
+  }
+  return {s,path,e}
+}
+
+const part1 = input => {
+  const {path} = parse(input)
+  const shortcuts = []
+  path.forEach(
+    ([x,y],i) => {
+      const directions = [[0,-2],[0,2],[-2,0],[2,0]]
+      for (let [dx, dy] of directions) {
+        const idx = path.findIndex(([_x,_y],_i)=>((_x==x+dx)&&(_y==y+dy)&&_i!=i+2))
+        const diff = (idx - i) - 2
+        if(diff>0) shortcuts.push(diff)
+      }
+    }
+  )
+  //return shortcuts.sort((a,b)=>a>b?1:-1)
+  return shortcuts.filter(x=>x>=100).length
+}
+
+module.exports = {part1}
