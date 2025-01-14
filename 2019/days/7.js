74f2895c59aee13a71575aac78fa24bdd126c798
David Sharp <hello@davidsharp.codes>
2025-01-14 22:45:33 +0000

    [2019] Completed 7-1

--- 2019/days/7.js
+++ 2019/days/7.js
@@ -1,19 +1,27 @@
-module.exports = { //incomplete solution
-  part1:data=>{
-    let inputs=0;
-    const max = parseInt('44444',5)
-    let highest = [0,0]
-    while(inputs<=max){
-      const inputArray = inputs.toString(5).padStart(5,'0').split('').map(Number)
-      const A = days[7].thruster(data,[inputArray[0],0])
-      const B = days[7].thruster(data,[inputArray[1],A])
-      const C = days[7].thruster(data,[inputArray[2],B])
-      const D = days[7].thruster(data,[inputArray[3],C])
-      const E = days[7].thruster(data,[inputArray[4],D])
-      if(E>highest[1])highest=[inputArray.join(),E]
-      console.log(inputArray,[A,B,C,D,E])
-      inputs++
-    }
-    return highest
-  }
+const {Interpreter} = require('../utils/intcode')
+
+const part1 = data => {
+  const phases = [0,1,2,3,4]
+  return runAmp(data,phases,0)
 }
+const runAmp = (program, remainingPhases, input) => {
+  if(remainingPhases.length == 0) return input
+  let highest = 0
+  for (let i = 0; i < remainingPhases.length; i++) {
+    const phase = remainingPhases[i]
+    let out = null
+    const int = new Interpreter(program,o=>out=o)
+    int.pipe(phase)
+    int.pipe(input)
+    int.run()
+    const x = runAmp(
+      program,
+      remainingPhases.toSpliced(i,1),
+      out
+    )
+    if (x > highest) highest = x
+  }
+  return highest
+}
+
+module.exports = {part1}
