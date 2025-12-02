7ff4db1d986caf91f567d79e0232cddb91b322b9
David Sharp <hello@davidsharp.codes>
2025-12-02 11:24:13 +0000

    [2025] Completed 2-2! ☃️

--- 2025/days/2.js
+++ 2025/days/2.js
@@ -11,4 +11,24 @@ const part1 = input => input.split(',').reduce((acc,range) => {
   return acc + idAcc
 },0)
 
-module.exports = {part1}
+const primes = [2,3,5,7]
+const part2 = input => input.split(',').reduce((acc,range) => {
+  let [i,max] = range.split('-').map(Number)
+  let idAcc = 0
+  while (i <= max) {
+    let invalid = false
+    const id = i.toString()
+    primes.forEach(p => {
+      if (id.length % p == 0) {
+        if(new RegExp(`(${id.slice(0,id.length/p)}){${p}}`).test(id)){
+          invalid = true
+        }
+      }
+    })
+    if(invalid) idAcc += i
+    i++
+  }
+  return acc + idAcc
+},0)
+
+module.exports = {part1,part2}
