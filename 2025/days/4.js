61bfaf210dcf75e0bd33f57b38db950be07ff4df
David Sharp <hello@davidsharp.codes>
2025-12-04 11:54:35 +0000

    [2025] Completed 4-2! ☃️

--- 2025/days/4.js
+++ 2025/days/4.js
@@ -29,4 +29,28 @@ const part1 = input => {
   return total
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const rolls = parse(input)
+  const remainingRolls = removeRolls(rolls)
+  return rolls.size - remainingRolls.size
+}
+const removeRolls = (rolls) => {
+  const nextRolls = new Set([...rolls])
+  nextRolls.forEach(roll => {
+    const [x,y] = roll.split(',').map(Number)
+    let count = 0
+    for (let i=-1;i<=1;i++) {
+      for (let j=-1;j<=1;j++) {
+        if (rolls.has(`${x+i},${y+j}`)) {
+          count++
+        }
+      }
+    }
+    if (count <= 4) {
+      nextRolls.delete(roll)
+    }
+  })
+  return rolls.size == nextRolls.size ? nextRolls : removeRolls(nextRolls)
+}
+
+module.exports = {part1,part2}
