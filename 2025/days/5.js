bdc65ed116b00124f176bb0c6caf9f7ff03402f8
David Sharp <hello@davidsharp.codes>
2025-12-05 12:40:06 +0000

    Not quite right 2025 5-2

--- 2025/days/5.js
+++ 2025/days/5.js
@@ -10,4 +10,31 @@ const part1 = input => {
   )?fresh+1:fresh),0)
 }
 
-module.exports = {part1}
+const part2 = input => {
+  const [ranges] = parse(input)
+  return ranges.toSorted((a,b)=>{
+    if (a[0] < b[0]) return -1
+    if (a[0] > b[0]) return 1
+    if (a[1] < b[1]) return -1
+    if (a[1] > b[1]) return 1
+    return 0
+  }).reduce((newRanges, range, i) => {
+    if(i==0) return [range]
+    const last = newRanges.pop()
+    if (last[0]<=range[0] && last[1]>= range[0]) {
+      newRanges.push([last[0],range[1]])
+    }
+    else newRanges.push(last,range)
+    return newRanges
+  }, []).reduce((count, range) => count + (range[1]-range[0]) + 1,0)
+  /*
+  return ranges.reduce((set, [min,max]) => {
+    for (let i = min; i <= max; i++) {
+      set.add(i)
+    }
+    return set
+  },new Set()).size
+  */
+}
+
+module.exports = {part1,part2}
